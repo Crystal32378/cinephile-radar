eb9ab08fd9bfc71262cce4a84bcabdc097b46195
Crystal Chang <crystalys.chang@gmail.com>
2026-06-30 10:57:30 +0800

    Add movie data part 04

---
+++ data-parts/part-04.js
@@ -0,0 +1,679 @@
+// Cinephile Radar data part 04
+window.MOVIE_DATA_PARTS = window.MOVIE_DATA_PARTS || [];
+window.MOVIE_DATA_PARTS.push([
+  {
+    "id": "berlin_2017_on_body_and_sou",
+    "festival": "berlin",
+    "year": 2017,
+    "title_en": "On Body and Soul",
+    "title_cn": "夢鹿情謎",
+    "director": "Ildikó Enyedi",
+    "country": "Hungary"
+  },
+  {
+    "id": "berlin_2016_fire_at_sea",
+    "festival": "berlin",
+    "year": 2016,
+    "title_en": "Fire at Sea",
+    "title_cn": "海上焰火",
+    "director": "Gianfranco Rosi",
+    "country": "Italy"
+  },
+  {
+    "id": "berlin_2015_taxi_(2015_film",
+    "festival": "berlin",
+    "year": 2015,
+    "title_en": "Taxi",
+    "title_cn": "計程人生",
+    "director": "Jafar Panahi",
+    "country": "Iran"
+  },
+  {
+    "id": "berlin_2014_black_coal,_thi",
+    "festival": "berlin",
+    "year": 2014,
+    "title_en": "Black Coal, Thin Ice",
+    "title_cn": "白日焰火",
+    "director": "Diao Yinan",
+    "country": "China"
+  },
+  {
+    "id": "berlin_2013_child's_pose_(f",
+    "festival": "berlin",
+    "year": 2013,
+    "title_en": "Child's Pose",
+    "title_cn": "愛的佔有慾",
+    "director": "Călin Peter Netzer",
+    "country": "Romania"
+  },
+  {
+    "id": "berlin_2012_caesar_must_die",
+    "festival": "berlin",
+    "year": 2012,
+    "title_en": "Caesar Must Die",
+    "title_cn": "凱撒必須死：舞台重生",
+    "director": "Paolo and Vittorio Taviani",
+    "country": "Italy"
+  },
+  {
+    "id": "berlin_2011_a_separation",
+    "festival": "berlin",
+    "year": 2011,
+    "title_en": "A Separation",
+    "title_cn": "分居風暴",
+    "director": "Asghar Farhadi",
+    "country": "Iran"
+  },
+  {
+    "id": "berlin_2010_honey_(2010_fil",
+    "festival": "berlin",
+    "year": 2010,
+    "title_en": "Honey",
+    "title_cn": "蜜",
+    "director": "Semih Kaplanoğlu",
+    "country": "Turkey"
+  },
+  {
+    "id": "berlin_2009_the_milk_of_sor",
+    "festival": "berlin",
+    "year": 2009,
+    "title_en": "The Milk of Sorrow",
+    "title_cn": "懼乳：傷心的奶水",
+    "director": "Claudia Llosa",
+    "country": "Spain, Peru"
+  },
+  {
+    "id": "berlin_2008_elite_squad",
+    "festival": "berlin",
+    "year": 2008,
+    "title_en": "Elite Squad",
+    "title_cn": "精銳部隊",
+    "director": "José Padilha",
+    "country": "Brazil"
+  },
+  {
+    "id": "berlin_2007_tuya's_marriage",
+    "festival": "berlin",
+    "year": 2007,
+    "title_en": "Tuya's Marriage",
+    "title_cn": "圖雅的婚事",
+    "director": "Wang Quan'an",
+    "country": "China"
+  },
+  {
+    "id": "berlin_2006_grbavica_(film)",
+    "festival": "berlin",
+    "year": 2006,
+    "title_en": "Grbavica",
+    "title_cn": "旅行之歌",
+    "director": "Jasmila Žbanić",
+    "country": "Bosnia and Herzegovina"
+  },
+  {
+    "id": "berlin_2005_u-carmen_ekhaye",
+    "festival": "berlin",
+    "year": 2005,
+    "title_en": "U-Carmen eKhayelitsha",
+    "title_cn": "U-Carmen eKhayelitsha",
+    "director": "Mark Dornford-May",
+    "country": "South Africa"
+  },
+  {
+    "id": "berlin_2004_head-on_(film)",
+    "festival": "berlin",
+    "year": 2004,
+    "title_en": "Head-On",
+    "title_cn": "愛無止盡",
+    "director": "Fatih Akin",
+    "country": "Germany, Turkey"
+  },
+  {
+    "id": "berlin_2003_in_this_world",
+    "festival": "berlin",
+    "year": 2003,
+    "title_en": "In This World",
+    "title_cn": "In This World",
+    "director": "Michael Winterbottom",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "berlin_2002_bloody_sunday_(",
+    "festival": "berlin",
+    "year": 2002,
+    "title_en": "Bloody Sunday",
+    "title_cn": "Bloody Sunday",
+    "director": "Paul Greengrass",
+    "country": "United Kingdom, Ireland"
+  },
+  {
+    "id": "berlin_2002_spirited_away",
+    "festival": "berlin",
+    "year": 2002,
+    "title_en": "Spirited Away",
+    "title_cn": "神隱少女",
+    "director": "Hayao Miyazaki",
+    "country": "Japan"
+  },
+  {
+    "id": "berlin_2001_intimacy_(2001_",
+    "festival": "berlin",
+    "year": 2001,
+    "title_en": "Intimacy",
+    "title_cn": "Intimacy",
+    "director": "Patrice Chéreau",
+    "country": "France"
+  },
+  {
+    "id": "berlin_2000_magnolia_(film)",
+    "festival": "berlin",
+    "year": 2000,
+    "title_en": "Magnolia",
+    "title_cn": "心靈角落",
+    "director": "Paul Thomas Anderson",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1999_the_thin_red_li",
+    "festival": "berlin",
+    "year": 1999,
+    "title_en": "The Thin Red Line",
+    "title_cn": "紅色警戒",
+    "director": "Terrence Malick",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1998_central_station",
+    "festival": "berlin",
+    "year": 1998,
+    "title_en": "Central Station",
+    "title_cn": "中央車站",
+    "director": "Walter Salles",
+    "country": "Brazil, France"
+  },
+  {
+    "id": "berlin_1997_the_people_vs._",
+    "festival": "berlin",
+    "year": 1997,
+    "title_en": "The People vs. Larry Flynt",
+    "title_cn": "情色風暴1997",
+    "director": "Miloš Forman",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1996_sense_and_sensi",
+    "festival": "berlin",
+    "year": 1996,
+    "title_en": "Sense and Sensibility",
+    "title_cn": "理性與感性",
+    "director": "Ang Lee",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1995_the_bait_(1995_",
+    "festival": "berlin",
+    "year": 1995,
+    "title_en": "The Bait",
+    "title_cn": "The Bait",
+    "director": "Bertrand Tavernier",
+    "country": "France"
+  },
+  {
+    "id": "berlin_1994_in_the_name_of_",
+    "festival": "berlin",
+    "year": 1994,
+    "title_en": "In the Name of the Father",
+    "title_cn": "以父之名",
+    "director": "Jim Sheridan",
+    "country": "United Kingdom, Ireland"
+  },
+  {
+    "id": "berlin_1993_the_wedding_ban",
+    "festival": "berlin",
+    "year": 1993,
+    "title_en": "The Wedding Banquet",
+    "title_cn": "囍宴",
+    "director": "Ang Lee",
+    "country": "Taiwan, United States"
+  },
+  {
+    "id": "berlin_1993_woman_sesame_oi",
+    "festival": "berlin",
+    "year": 1993,
+    "title_en": "Woman Sesame Oil Maker",
+    "title_cn": "香魂女",
+    "director": "Xie Fei",
+    "country": "China"
+  },
+  {
+    "id": "berlin_1992_grand_canyon_(1",
+    "festival": "berlin",
+    "year": 1992,
+    "title_en": "Grand Canyon",
+    "title_cn": "Grand Canyon",
+    "director": "Lawrence Kasdan",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1991_the_house_of_sm",
+    "festival": "berlin",
+    "year": 1991,
+    "title_en": "The House of Smiles",
+    "title_cn": "The House of Smiles",
+    "director": "Marco Ferreri",
+    "country": "Italy"
+  },
+  {
+    "id": "berlin_1990_larks_on_a_stri",
+    "festival": "berlin",
+    "year": 1990,
+    "title_en": "Larks on a String",
+    "title_cn": "Larks on a String",
+    "director": "Jiří Menzel",
+    "country": "Czechoslovakia"
+  },
+  {
+    "id": "berlin_1990_music_box_(film",
+    "festival": "berlin",
+    "year": 1990,
+    "title_en": "Music Box",
+    "title_cn": "Music Box",
+    "director": "Costa-Gavras",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1989_rain_man",
+    "festival": "berlin",
+    "year": 1989,
+    "title_en": "Rain Man",
+    "title_cn": "雨人",
+    "director": "Barry Levinson",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1988_red_sorghum_(fi",
+    "festival": "berlin",
+    "year": 1988,
+    "title_en": "Red Sorghum",
+    "title_cn": "紅高粱",
+    "director": "Zhang Yimou",
+    "country": "China"
+  },
+  {
+    "id": "berlin_1987_the_theme",
+    "festival": "berlin",
+    "year": 1987,
+    "title_en": "The Theme",
+    "title_cn": "The Theme",
+    "director": "Gleb Panfilov",
+    "country": "Soviet Union"
+  },
+  {
+    "id": "berlin_1986_stammheim_(film",
+    "festival": "berlin",
+    "year": 1986,
+    "title_en": "Stammheim",
+    "title_cn": "Stammheim",
+    "director": "Reinhard Hauff",
+    "country": "West Germany"
+  },
+  {
+    "id": "berlin_1985_wetherby_(film)",
+    "festival": "berlin",
+    "year": 1985,
+    "title_en": "Wetherby",
+    "title_cn": "Wetherby",
+    "director": "David Hare",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "berlin_1985_the_woman_and_t",
+    "festival": "berlin",
+    "year": 1985,
+    "title_en": "The Woman and the Stranger",
+    "title_cn": "The Woman and the Stranger",
+    "director": "Rainer Simon",
+    "country": "East Germany"
+  },
+  {
+    "id": "berlin_1984_love_streams_(f",
+    "festival": "berlin",
+    "year": 1984,
+    "title_en": "Love Streams",
+    "title_cn": "暗湧",
+    "director": "John Cassavetes",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1983_ascendancy_(fil",
+    "festival": "berlin",
+    "year": 1983,
+    "title_en": "Ascendancy",
+    "title_cn": "Ascendancy",
+    "director": "Edward Bennett",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "berlin_1983_la_colmena_(fil",
+    "festival": "berlin",
+    "year": 1983,
+    "title_en": "La colmena",
+    "title_cn": "La colmena",
+    "director": "Mario Camus",
+    "country": "Spain"
+  },
+  {
+    "id": "berlin_1982_veronika_voss",
+    "festival": "berlin",
+    "year": 1982,
+    "title_en": "Veronika Voss",
+    "title_cn": "Veronika Voss",
+    "director": "Rainer Werner Fassbinder",
+    "country": "West Germany"
+  },
+  {
+    "id": "berlin_1981_deprisa,_depris",
+    "festival": "berlin",
+    "year": 1981,
+    "title_en": "Deprisa, deprisa",
+    "title_cn": "Deprisa, deprisa",
+    "director": "Carlos Saura",
+    "country": "Spain"
+  },
+  {
+    "id": "berlin_1980_heartland_(1979",
+    "festival": "berlin",
+    "year": 1980,
+    "title_en": "Heartland",
+    "title_cn": "Heartland",
+    "director": "Richard Pearce",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1980_palermo_or_wolf",
+    "festival": "berlin",
+    "year": 1980,
+    "title_en": "Palermo or Wolfsburg",
+    "title_cn": "Palermo or Wolfsburg",
+    "director": "Werner Schroeter",
+    "country": "West Germany"
+  },
+  {
+    "id": "berlin_1979_david_(1979_fil",
+    "festival": "berlin",
+    "year": 1979,
+    "title_en": "David",
+    "title_cn": "David",
+    "director": "Peter Lilienthal",
+    "country": "West Germany"
+  },
+  {
+    "id": "berlin_1978_ascensor",
+    "festival": "berlin",
+    "year": 1978,
+    "title_en": "Ascensor",
+    "title_cn": "電梯",
+    "director": "Tomás Muñoz",
+    "country": "Spain"
+  },
+  {
+    "id": "berlin_1978_las_truchas",
+    "festival": "berlin",
+    "year": 1978,
+    "title_en": "Las truchas",
+    "title_cn": "Las truchas",
+    "director": "José Luis García Sánchez",
+    "country": "Spain"
+  },
+  {
+    "id": "berlin_1978_what_max_said",
+    "festival": "berlin",
+    "year": 1978,
+    "title_en": "What Max Said",
+    "title_cn": "What Max Said",
+    "director": "Emilio Martínez-Lázaro",
+    "country": "Spain"
+  },
+  {
+    "id": "berlin_1977_the_ascent_(197",
+    "festival": "berlin",
+    "year": 1977,
+    "title_en": "The Ascent",
+    "title_cn": "The Ascent",
+    "director": "Larisa Shepitko",
+    "country": "Soviet Union"
+  },
+  {
+    "id": "berlin_1976_buffalo_bill_an",
+    "festival": "berlin",
+    "year": 1976,
+    "title_en": "Buffalo Bill and the Indians, or Sitting Bull's History Lesson",
+    "title_cn": "西塞英雄譜",
+    "director": "Robert Altman",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1975_adoption_(film)",
+    "festival": "berlin",
+    "year": 1975,
+    "title_en": "Adoption",
+    "title_cn": "Adoption",
+    "director": "Márta Mészáros",
+    "country": "Hungary"
+  },
+  {
+    "id": "berlin_1974_the_apprentices",
+    "festival": "berlin",
+    "year": 1974,
+    "title_en": "The Apprenticeship of Duddy Kravitz",
+    "title_cn": "The Apprenticeship of Duddy Kravitz",
+    "director": "Ted Kotcheff",
+    "country": "Canada"
+  },
+  {
+    "id": "berlin_1973_distant_thunder",
+    "festival": "berlin",
+    "year": 1973,
+    "title_en": "Distant Thunder",
+    "title_cn": "遙遠的雷聲",
+    "director": "Satyajit Ray",
+    "country": "India"
+  },
+  {
+    "id": "berlin_1972_the_canterbury_",
+    "festival": "berlin",
+    "year": 1972,
+    "title_en": "The Canterbury Tales",
+    "title_cn": "坎特伯雷故事",
+    "director": "Pier Paolo Pasolini",
+    "country": "Italy"
+  },
+  {
+    "id": "berlin_1971_the_garden_of_t",
+    "festival": "berlin",
+    "year": 1971,
+    "title_en": "The Garden of the Finzi-Continis",
+    "title_cn": "The Garden of the Finzi-Continis",
+    "director": "Vittorio De Sica",
+    "country": "West Germany, Italy"
+  },
+  {
+    "id": "berlin_1969_early_works_(fi",
+    "festival": "berlin",
+    "year": 1969,
+    "title_en": "Early Works",
+    "title_cn": "Early Works",
+    "director": "Želimir Žilnik",
+    "country": "Yugoslavia"
+  },
+  {
+    "id": "berlin_1968_who_saw_him_die",
+    "festival": "berlin",
+    "year": 1968,
+    "title_en": "Who Saw Him Die?",
+    "title_cn": "Who Saw Him Die?",
+    "director": "Jan Troell",
+    "country": "Sweden"
+  },
+  {
+    "id": "berlin_1967_le_départ",
+    "festival": "berlin",
+    "year": 1967,
+    "title_en": "Le Départ",
+    "title_cn": "Le Départ",
+    "director": "Jerzy Skolimowski",
+    "country": "Belgium"
+  },
+  {
+    "id": "berlin_1966_cul-de-sac_(196",
+    "festival": "berlin",
+    "year": 1966,
+    "title_en": "Cul-de-Sac",
+    "title_cn": "Cul-de-Sac",
+    "director": "Roman Polanski",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "berlin_1965_alphaville_(fil",
+    "festival": "berlin",
+    "year": 1965,
+    "title_en": "Alphaville",
+    "title_cn": "阿爾發城",
+    "director": "Jean-Luc Godard",
+    "country": "France, Italy"
+  },
+  {
+    "id": "berlin_1964_dry_summer",
+    "festival": "berlin",
+    "year": 1964,
+    "title_en": "Dry Summer",
+    "title_cn": "Dry Summer",
+    "director": "Metin Erksan",
+    "country": "Turkey"
+  },
+  {
+    "id": "berlin_1963_bushido,_samura",
+    "festival": "berlin",
+    "year": 1963,
+    "title_en": "Bushido, Samurai Saga",
+    "title_cn": "Bushido, Samurai Saga",
+    "director": "Tadashi Imai",
+    "country": "Japan"
+  },
+  {
+    "id": "berlin_1963_to_bed_or_not_t",
+    "festival": "berlin",
+    "year": 1963,
+    "title_en": "To Bed or Not to Bed",
+    "title_cn": "To Bed or Not to Bed",
+    "director": "Gian Luigi Polidoro",
+    "country": "Italy"
+  },
+  {
+    "id": "berlin_1962_a_kind_of_lovin",
+    "festival": "berlin",
+    "year": 1962,
+    "title_en": "A Kind of Loving",
+    "title_cn": "A Kind of Loving",
+    "director": "John Schlesinger",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "berlin_1961_la_notte",
+    "festival": "berlin",
+    "year": 1961,
+    "title_en": "La Notte",
+    "title_cn": "夜",
+    "director": "Michelangelo Antonioni",
+    "country": "Italy"
+  },
+  {
+    "id": "berlin_1960_el_lazarillo_de",
+    "festival": "berlin",
+    "year": 1960,
+    "title_en": "El Lazarillo de Tormes",
+    "title_cn": "小癩子",
+    "director": "César Fernández Ardavín",
+    "country": "Spain"
+  },
+  {
+    "id": "berlin_1959_les_cousins_(fi",
+    "festival": "berlin",
+    "year": 1959,
+    "title_en": "Les Cousins",
+    "title_cn": "表兄弟",
+    "director": "Claude Chabrol",
+    "country": "France"
+  },
+  {
+    "id": "berlin_1958_wild_strawberri",
+    "festival": "berlin",
+    "year": 1958,
+    "title_en": "Wild Strawberries",
+    "title_cn": "野草莓",
+    "director": "Ingmar Bergman",
+    "country": "Sweden"
+  },
+  {
+    "id": "berlin_1957_12_angry_men_(1",
+    "festival": "berlin",
+    "year": 1957,
+    "title_en": "12 Angry Men",
+    "title_cn": "十二怒漢",
+    "director": "Sidney Lumet",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1956_invitation_to_t",
+    "festival": "berlin",
+    "year": 1956,
+    "title_en": "Invitation to the Dance",
+    "title_cn": "Invitation to the Dance",
+    "director": "Gene Kelly",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1955_die_ratten",
+    "festival": "berlin",
+    "year": 1955,
+    "title_en": "Die Ratten",
+    "title_cn": "Die Ratten",
+    "director": "Robert Siodmak",
+    "country": "West Germany"
+  },
+  {
+    "id": "berlin_1954_hobson's_choice",
+    "festival": "berlin",
+    "year": 1954,
+    "title_en": "Hobson's Choice",
+    "title_cn": "Hobson's Choice",
+    "director": "David Lean",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "berlin_1953_the_wages_of_fe",
+    "festival": "berlin",
+    "year": 1953,
+    "title_en": "The Wages of Fear",
+    "title_cn": "恐怖的報酬",
+    "director": "Henri-Georges Clouzot",
+    "country": "France, Italy"
+  },
+  {
+    "id": "berlin_1952_one_summer_of_h",
+    "festival": "berlin",
+    "year": 1952,
+    "title_en": "One Summer of Happiness",
+    "title_cn": "One Summer of Happiness",
+    "director": "Arne Mattsson",
+    "country": "Sweden"
+  },
+  {
+    "id": "berlin_1951_four_in_a_jeep",
+    "festival": "berlin",
+    "year": 1951,
+    "title_en": "Four in a Jeep (Drama)",
+    "title_cn": "Four in a Jeep (Drama)",
+    "director": "Leopold Lindtberg",
+    "country": "Switzerland"
+  }
+]);
