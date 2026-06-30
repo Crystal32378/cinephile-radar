fa702cca696c641cedebcdf52c74bf9b1a080d7b
Crystal Chang <crystalys.chang@gmail.com>
2026-06-30 10:51:42 +0800

    Add movie data part 02

---
+++ data-parts/part-02.js
@@ -0,0 +1,679 @@
+// Cinephile Radar data part 02
+window.MOVIE_DATA_PARTS = window.MOVIE_DATA_PARTS || [];
+window.MOVIE_DATA_PARTS.push([
+  {
+    "id": "cannes_1958_the_cranes_are_",
+    "festival": "cannes",
+    "year": 1958,
+    "title_en": "The Cranes Are Flying",
+    "title_cn": "雁南飛",
+    "director": "Mikhail Kalatozov",
+    "country": "Soviet Union"
+  },
+  {
+    "id": "cannes_1957_friendly_persua",
+    "festival": "cannes",
+    "year": 1957,
+    "title_en": "Friendly Persuasion",
+    "title_cn": "Friendly Persuasion",
+    "director": "William Wyler",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1956_the_silent_worl",
+    "festival": "cannes",
+    "year": 1956,
+    "title_en": "The Silent World",
+    "title_cn": "The Silent World",
+    "director": "Jacques Cousteau Louis Malle",
+    "country": "France"
+  },
+  {
+    "id": "cannes_1955_marty_(film)",
+    "festival": "cannes",
+    "year": 1955,
+    "title_en": "Marty §",
+    "title_cn": "馬蒂",
+    "director": "Delbert Mann",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1954_gate_of_hell_(f",
+    "festival": "cannes",
+    "year": 1954,
+    "title_en": "Gate of Hell",
+    "title_cn": "地獄門",
+    "director": "Teinosuke Kinugasa",
+    "country": "Japan"
+  },
+  {
+    "id": "cannes_1953_the_wages_of_fe",
+    "festival": "cannes",
+    "year": 1953,
+    "title_en": "The Wages of Fear",
+    "title_cn": "恐怖的報酬",
+    "director": "Henri-Georges Clouzot",
+    "country": "France"
+  },
+  {
+    "id": "cannes_1952_othello_(1951_f",
+    "festival": "cannes",
+    "year": 1952,
+    "title_en": "Othello",
+    "title_cn": "Othello",
+    "director": "Orson Welles",
+    "country": "Italy, Morocco, United States"
+  },
+  {
+    "id": "cannes_1952_two_cents_worth",
+    "festival": "cannes",
+    "year": 1952,
+    "title_en": "Two Cents Worth of Hope",
+    "title_cn": "Two Cents Worth of Hope",
+    "director": "Renato Castellani",
+    "country": "Italy"
+  },
+  {
+    "id": "cannes_1951_miracle_in_mila",
+    "festival": "cannes",
+    "year": 1951,
+    "title_en": "Miracle in Milan",
+    "title_cn": "Miracle in Milan",
+    "director": "Vittorio De Sica",
+    "country": "Italy"
+  },
+  {
+    "id": "cannes_1951_miss_julie_(195",
+    "festival": "cannes",
+    "year": 1951,
+    "title_en": "Miss Julie",
+    "title_cn": "Miss Julie",
+    "director": "Alf Sjöberg",
+    "country": "Sweden"
+  },
+  {
+    "id": "cannes_1949_the_third_man",
+    "festival": "cannes",
+    "year": 1949,
+    "title_en": "The Third Man",
+    "title_cn": "黑獄亡魂",
+    "director": "Carol Reed",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "cannes_1947_antoine_and_ant",
+    "festival": "cannes",
+    "year": 1947,
+    "title_en": "Antoine and Antoinette (Best Psychological & Love Film)",
+    "title_cn": "Antoine and Antoinette (Best Psychological & Love Film)",
+    "director": "Jacques Becker",
+    "country": "France"
+  },
+  {
+    "id": "cannes_1947_crossfire_(film",
+    "festival": "cannes",
+    "year": 1947,
+    "title_en": "Crossfire (Best Social Film)",
+    "title_cn": "Crossfire (Best Social Film)",
+    "director": "Edward Dmytryk",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1947_dumbo",
+    "festival": "cannes",
+    "year": 1947,
+    "title_en": "Dumbo (Best Animation Design)",
+    "title_cn": "小飛象",
+    "director": "Ben Sharpsteen",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1947_the_damned_(194",
+    "festival": "cannes",
+    "year": 1947,
+    "title_en": "The Damned (Best Adventure & Crime Film)",
+    "title_cn": "The Damned (Best Adventure & Crime Film)",
+    "director": "René Clément",
+    "country": "France"
+  },
+  {
+    "id": "cannes_1947_ziegfeld_follie",
+    "festival": "cannes",
+    "year": 1947,
+    "title_en": "Ziegfeld Follies (Best Musical Comedy)",
+    "title_cn": "Ziegfeld Follies (Best Musical Comedy)",
+    "director": "Vincente Minnelli",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1946_brief_encounter",
+    "festival": "cannes",
+    "year": 1946,
+    "title_en": "Brief Encounter",
+    "title_cn": "Brief Encounter",
+    "director": "David Lean",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "cannes_1946_the_last_chance",
+    "festival": "cannes",
+    "year": 1946,
+    "title_en": "The Last Chance",
+    "title_cn": "The Last Chance",
+    "director": "Leopold Lindtberg",
+    "country": "Switzerland"
+  },
+  {
+    "id": "cannes_1946_the_lost_weeken",
+    "festival": "cannes",
+    "year": 1946,
+    "title_en": "The Lost Weekend",
+    "title_cn": "失去的週末",
+    "director": "Billy Wilder",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1946_maría_candelari",
+    "festival": "cannes",
+    "year": 1946,
+    "title_en": "María Candelaria",
+    "title_cn": "María Candelaria",
+    "director": "Emilio Fernández",
+    "country": "Mexico"
+  },
+  {
+    "id": "cannes_1946_men_without_win",
+    "festival": "cannes",
+    "year": 1946,
+    "title_en": "Men Without Wings",
+    "title_cn": "Men Without Wings",
+    "director": "František Čáp",
+    "country": "Czechoslovakia"
+  },
+  {
+    "id": "cannes_1946_neecha_nagar",
+    "festival": "cannes",
+    "year": 1946,
+    "title_en": "Neecha Nagar",
+    "title_cn": "Neecha Nagar",
+    "director": "Chetan Anand",
+    "country": "India"
+  },
+  {
+    "id": "cannes_1946_pastoral_sympho",
+    "festival": "cannes",
+    "year": 1946,
+    "title_en": "Pastoral Symphony",
+    "title_cn": "Pastoral Symphony",
+    "director": "Jean Delannoy",
+    "country": "France"
+  },
+  {
+    "id": "cannes_1946_the_red_meadows",
+    "festival": "cannes",
+    "year": 1946,
+    "title_en": "The Red Meadows",
+    "title_cn": "The Red Meadows",
+    "director": "Bodil Ipsen Lau Lauritzen Jr.",
+    "country": "Denmark"
+  },
+  {
+    "id": "cannes_1946_rome,_open_city",
+    "festival": "cannes",
+    "year": 1946,
+    "title_en": "Rome, Open City",
+    "title_cn": "不設防城市",
+    "director": "Roberto Rossellini",
+    "country": "Italy"
+  },
+  {
+    "id": "cannes_1946_torment_(1944_f",
+    "festival": "cannes",
+    "year": 1946,
+    "title_en": "Torment",
+    "title_cn": "Torment",
+    "director": "Alf Sjöberg",
+    "country": "Sweden"
+  },
+  {
+    "id": "cannes_1946_the_turning_poi",
+    "festival": "cannes",
+    "year": 1946,
+    "title_en": "The Turning Point",
+    "title_cn": "The Turning Point",
+    "director": "Fridrikh Ermler",
+    "country": "Soviet Union"
+  },
+  {
+    "id": "venice_2025_father_mother_s",
+    "festival": "venice",
+    "year": 2025,
+    "title_en": "Father Mother Sister Brother",
+    "title_cn": "家庭日",
+    "director": "Jim Jarmusch",
+    "country": "United States, Ireland, France"
+  },
+  {
+    "id": "venice_2025_werner_herzog",
+    "festival": "venice",
+    "year": 2025,
+    "title_en": "Werner Herzog and Kim Novak",
+    "title_cn": "韋納·荷索",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2024_the_room_next_d",
+    "festival": "venice",
+    "year": 2024,
+    "title_en": "The Room Next Door",
+    "title_cn": "隔壁的房間",
+    "director": "Pedro Almodóvar",
+    "country": "Spain"
+  },
+  {
+    "id": "venice_2024_peter_weir",
+    "festival": "venice",
+    "year": 2024,
+    "title_en": "Peter Weir and Sigourney Weaver",
+    "title_cn": "彼得·威爾",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2023_poor_things_(fi",
+    "festival": "venice",
+    "year": 2023,
+    "title_en": "Poor Things",
+    "title_cn": "可憐的東西",
+    "director": "Yorgos Lanthimos",
+    "country": "Ireland, United Kingdom, United States"
+  },
+  {
+    "id": "venice_2023_liliana_cavani",
+    "festival": "venice",
+    "year": 2023,
+    "title_en": "Liliana Cavani and Tony Leung Chiu-wai",
+    "title_cn": "莉莉安娜·卡瓦尼",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2022_all_the_beauty_",
+    "festival": "venice",
+    "year": 2022,
+    "title_en": "All the Beauty and the Bloodshed",
+    "title_cn": "所有的美麗與血淚",
+    "director": "Laura Poitras",
+    "country": "United States"
+  },
+  {
+    "id": "venice_2022_catherine_deneu",
+    "festival": "venice",
+    "year": 2022,
+    "title_en": "Catherine Deneuve and Paul Schrader",
+    "title_cn": "凱薩琳·丹妮芙",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2021_happening_(film",
+    "festival": "venice",
+    "year": 2021,
+    "title_en": "Happening §",
+    "title_cn": "正發生",
+    "director": "Audrey Diwan",
+    "country": "France"
+  },
+  {
+    "id": "venice_2021_roberto_benigni",
+    "festival": "venice",
+    "year": 2021,
+    "title_en": "Roberto Benigni and Jamie Lee Curtis",
+    "title_cn": "羅貝托·貝尼尼",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2020_nomadland",
+    "festival": "venice",
+    "year": 2020,
+    "title_en": "Nomadland",
+    "title_cn": "游牧人生",
+    "director": "Chloé Zhao",
+    "country": "United States"
+  },
+  {
+    "id": "venice_2020_ann_hui",
+    "festival": "venice",
+    "year": 2020,
+    "title_en": "Ann Hui and Tilda Swinton",
+    "title_cn": "許鞍華",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2019_joker_(2019_fil",
+    "festival": "venice",
+    "year": 2019,
+    "title_en": "Joker",
+    "title_cn": "小丑",
+    "director": "Todd Phillips",
+    "country": "United States"
+  },
+  {
+    "id": "venice_2019_julie_andrews",
+    "festival": "venice",
+    "year": 2019,
+    "title_en": "Julie Andrews and Pedro Almodóvar",
+    "title_cn": "茱莉·安德魯絲",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2018_roma_(2018_film",
+    "festival": "venice",
+    "year": 2018,
+    "title_en": "Roma",
+    "title_cn": "羅馬",
+    "director": "Alfonso Cuarón",
+    "country": "Mexico, United States"
+  },
+  {
+    "id": "venice_2018_david_cronenber",
+    "festival": "venice",
+    "year": 2018,
+    "title_en": "David Cronenberg and Vanessa Redgrave",
+    "title_cn": "大衛·柯能堡",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2017_the_shape_of_wa",
+    "festival": "venice",
+    "year": 2017,
+    "title_en": "The Shape of Water",
+    "title_cn": "水底情深",
+    "director": "Guillermo del Toro",
+    "country": "United States, Mexico"
+  },
+  {
+    "id": "venice_2017_jane_fonda",
+    "festival": "venice",
+    "year": 2017,
+    "title_en": "Jane Fonda and Robert Redford",
+    "title_cn": "珍·芳達",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2016_the_woman_who_l",
+    "festival": "venice",
+    "year": 2016,
+    "title_en": "The Woman Who Left",
+    "title_cn": "離去的女人",
+    "director": "Lav Diaz",
+    "country": "Philippines"
+  },
+  {
+    "id": "venice_2016_jean-paul_belmo",
+    "festival": "venice",
+    "year": 2016,
+    "title_en": "Jean-Paul Belmondo and Jerzy Skolimowski",
+    "title_cn": "楊波·貝蒙",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2015_from_afar_(film",
+    "festival": "venice",
+    "year": 2015,
+    "title_en": "From Afar",
+    "title_cn": "遠方禁戀",
+    "director": "Lorenzo Vigas",
+    "country": "Venezuela"
+  },
+  {
+    "id": "venice_2015_bertrand_tavern",
+    "festival": "venice",
+    "year": 2015,
+    "title_en": "Bertrand Tavernier",
+    "title_cn": "貝特杭·塔維涅",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2014_a_pigeon_sat_on",
+    "festival": "venice",
+    "year": 2014,
+    "title_en": "A Pigeon Sat on a Branch Reflecting on Existence",
+    "title_cn": "鴿子在樹枝上沉思",
+    "director": "Roy Andersson",
+    "country": "Sweden"
+  },
+  {
+    "id": "venice_2014_thelma_schoonma",
+    "festival": "venice",
+    "year": 2014,
+    "title_en": "Thelma Schoonmaker and Frederick Wiseman",
+    "title_cn": "Thelma Schoonmaker and Frederick Wiseman",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2013_sacro_gra",
+    "festival": "venice",
+    "year": 2013,
+    "title_en": "Sacro GRA",
+    "title_cn": "一條大路通羅馬",
+    "director": "Gianfranco Rosi",
+    "country": "Italy"
+  },
+  {
+    "id": "venice_2013_william_friedki",
+    "festival": "venice",
+    "year": 2013,
+    "title_en": "William Friedkin",
+    "title_cn": "威廉·佛雷金",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2012_pietà_(film)",
+    "festival": "venice",
+    "year": 2012,
+    "title_en": "Pietà",
+    "title_cn": "聖殤",
+    "director": "Kim Ki-duk",
+    "country": "South Korea"
+  },
+  {
+    "id": "venice_2012_francesco_rosi",
+    "festival": "venice",
+    "year": 2012,
+    "title_en": "Francesco Rosi",
+    "title_cn": "法蘭西斯科·羅西",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2011_faust_(2011_fil",
+    "festival": "venice",
+    "year": 2011,
+    "title_en": "Faust §",
+    "title_cn": "浮士德－魔鬼的誘惑",
+    "director": "Alexander Sokurov",
+    "country": "Russia"
+  },
+  {
+    "id": "venice_2011_marco_bellocchi",
+    "festival": "venice",
+    "year": 2011,
+    "title_en": "Marco Bellocchio",
+    "title_cn": "馬可·貝洛奇歐",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2010_somewhere_(film",
+    "festival": "venice",
+    "year": 2010,
+    "title_en": "Somewhere §",
+    "title_cn": "迷失某地",
+    "director": "Sofia Coppola",
+    "country": "United States"
+  },
+  {
+    "id": "venice_2010_john_woo",
+    "festival": "venice",
+    "year": 2010,
+    "title_en": "John Woo",
+    "title_cn": "吳宇森",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2009_lebanon_(film)",
+    "festival": "venice",
+    "year": 2009,
+    "title_en": "Lebanon",
+    "title_cn": "黎巴嫩",
+    "director": "Samuel Maoz",
+    "country": "Israel"
+  },
+  {
+    "id": "venice_2009_john_lasseter",
+    "festival": "venice",
+    "year": 2009,
+    "title_en": "John Lasseter, Brad Bird, Pete Docter, Andrew Stanton, and Lee Unkrich",
+    "title_cn": "約翰·拉薩特",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2008_the_wrestler_(2",
+    "festival": "venice",
+    "year": 2008,
+    "title_en": "The Wrestler",
+    "title_cn": "力挽狂瀾",
+    "director": "Darren Aronofsky",
+    "country": "United States"
+  },
+  {
+    "id": "venice_2008_ermanno_olmi",
+    "festival": "venice",
+    "year": 2008,
+    "title_en": "Ermanno Olmi",
+    "title_cn": "Ermanno Olmi",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2007_lust,_caution",
+    "festival": "venice",
+    "year": 2007,
+    "title_en": "Lust, Caution",
+    "title_cn": "色，戒",
+    "director": "Ang Lee",
+    "country": "Taiwan, China, United States"
+  },
+  {
+    "id": "venice_2007_tim_burton",
+    "festival": "venice",
+    "year": 2007,
+    "title_en": "Tim Burton and Bernardo Bertolucci (for the last 75 years of the history of cinema)",
+    "title_cn": "提姆·波頓",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2006_still_life_(200",
+    "festival": "venice",
+    "year": 2006,
+    "title_en": "Still Life",
+    "title_cn": "三峽好人",
+    "director": "Jia Zhangke",
+    "country": "China"
+  },
+  {
+    "id": "venice_2006_david_lynch",
+    "festival": "venice",
+    "year": 2006,
+    "title_en": "David Lynch",
+    "title_cn": "大衛·林區",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2005_brokeback_mount",
+    "festival": "venice",
+    "year": 2005,
+    "title_en": "Brokeback Mountain",
+    "title_cn": "斷背山",
+    "director": "Ang Lee",
+    "country": "United States"
+  },
+  {
+    "id": "venice_2005_hayao_miyazaki",
+    "festival": "venice",
+    "year": 2005,
+    "title_en": "Hayao Miyazaki and Stefania Sandrelli",
+    "title_cn": "宮崎駿",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2004_vera_drake",
+    "festival": "venice",
+    "year": 2004,
+    "title_en": "Vera Drake",
+    "title_cn": "天使薇拉卓克",
+    "director": "Mike Leigh",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "venice_2004_stanley_donen",
+    "festival": "venice",
+    "year": 2004,
+    "title_en": "Stanley Donen and Manoel de Oliveira",
+    "title_cn": "史丹利·杜寧",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2003_the_return_(200",
+    "festival": "venice",
+    "year": 2003,
+    "title_en": "The Return",
+    "title_cn": "歸鄉",
+    "director": "Andrey Zvyagintsev",
+    "country": "Russia"
+  },
+  {
+    "id": "venice_2003_dino_de_laurent",
+    "festival": "venice",
+    "year": 2003,
+    "title_en": "Dino De Laurentiis and Omar Sharif",
+    "title_cn": "迪諾·德勞倫提斯",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2002_the_magdalene_s",
+    "festival": "venice",
+    "year": 2002,
+    "title_en": "The Magdalene Sisters",
+    "title_cn": "The Magdalene Sisters",
+    "director": "Peter Mullan",
+    "country": "Ireland, United Kingdom"
+  },
+  {
+    "id": "venice_2002_dino_risi",
+    "festival": "venice",
+    "year": 2002,
+    "title_en": "Dino Risi",
+    "title_cn": "迪諾·里西",
+    "director": "",
+    "country": ""
+  }
+]);
