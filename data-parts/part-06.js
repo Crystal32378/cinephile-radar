8c33c57b6951f5d1e2cd9e1d49e607e9583bda67
Crystal Chang <crystalys.chang@gmail.com>
2026-06-30 10:55:15 +0800

    Add movie data part 06

---
+++ data-parts/part-06.js
@@ -0,0 +1,247 @@
+// Cinephile Radar data part 06
+window.MOVIE_DATA_PARTS = window.MOVIE_DATA_PARTS || [];
+window.MOVIE_DATA_PARTS.push([
+  {
+    "id": "oscar_1954_on_the_waterfro",
+    "festival": "oscar",
+    "year": 1954,
+    "title_en": "On the Waterfront",
+    "title_cn": "岸上風雲",
+    "director": "Sam Spiegel",
+    "country": ""
+  },
+  {
+    "id": "oscar_1953_from_here_to_et",
+    "festival": "oscar",
+    "year": 1953,
+    "title_en": "From Here to Eternity",
+    "title_cn": "亂世忠魂",
+    "director": "Buddy Adler",
+    "country": ""
+  },
+  {
+    "id": "oscar_1952_the_greatest_sh",
+    "festival": "oscar",
+    "year": 1952,
+    "title_en": "The Greatest Show on Earth",
+    "title_cn": "戲中之王",
+    "director": "Cecil B. DeMille (Paramount)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1951_an_american_in_",
+    "festival": "oscar",
+    "year": 1951,
+    "title_en": "An American in Paris",
+    "title_cn": "花都舞影",
+    "director": "Arthur Freed (Metro-Goldwyn-Mayer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1950_all_about_eve",
+    "festival": "oscar",
+    "year": 1950,
+    "title_en": "All About Eve",
+    "title_cn": "彗星美人",
+    "director": "20th Century-Fox (Darryl F. Zanuck, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1949_all_the_king's_",
+    "festival": "oscar",
+    "year": 1949,
+    "title_en": "All the King's Men",
+    "title_cn": "當代奸雄",
+    "director": "Columbia (Robert Rossen, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1948_hamlet_(1948_fi",
+    "festival": "oscar",
+    "year": 1948,
+    "title_en": "Hamlet",
+    "title_cn": "王子復仇記",
+    "director": "J. Arthur Rank-Two Cities Films (Laurence Olivier, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1947_gentleman's_agr",
+    "festival": "oscar",
+    "year": 1947,
+    "title_en": "Gentleman's Agreement",
+    "title_cn": "君子協定",
+    "director": "20th Century-Fox (Darryl F. Zanuck, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1946_the_best_years_",
+    "festival": "oscar",
+    "year": 1946,
+    "title_en": "The Best Years of Our Lives",
+    "title_cn": "黃金時代",
+    "director": "Samuel Goldwyn Productions (Samuel Goldwyn, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1945_the_lost_weeken",
+    "festival": "oscar",
+    "year": 1945,
+    "title_en": "The Lost Weekend",
+    "title_cn": "失去的週末",
+    "director": "Paramount (Charles Brackett, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1944_going_my_way",
+    "festival": "oscar",
+    "year": 1944,
+    "title_en": "Going My Way",
+    "title_cn": "與我同行",
+    "director": "Paramount (Leo McCarey, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1943_casablanca_(fil",
+    "festival": "oscar",
+    "year": 1943,
+    "title_en": "Casablanca",
+    "title_cn": "北非諜影",
+    "director": "Warner Bros. (Hal B. Wallis, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1942_mrs._miniver",
+    "festival": "oscar",
+    "year": 1942,
+    "title_en": "Mrs. Miniver",
+    "title_cn": "忠勇之家",
+    "director": "Metro-Goldwyn-Mayer (Sidney Franklin, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1941_how_green_was_m",
+    "festival": "oscar",
+    "year": 1941,
+    "title_en": "How Green Was My Valley",
+    "title_cn": "翡翠谷",
+    "director": "20th Century-Fox (Darryl F. Zanuck, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1940_rebecca_(1940_f",
+    "festival": "oscar",
+    "year": 1940,
+    "title_en": "Rebecca",
+    "title_cn": "蝴蝶夢",
+    "director": "Selznick International Pictures (David O. Selznick, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1939_gone_with_the_w",
+    "festival": "oscar",
+    "year": 1939,
+    "title_en": "Gone with the Wind",
+    "title_cn": "亂世佳人",
+    "director": "Metro-Goldwyn-Mayer (David O. Selznick, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1938_you_can't_take_",
+    "festival": "oscar",
+    "year": 1938,
+    "title_en": "You Can't Take It with You",
+    "title_cn": "浮生若夢",
+    "director": "Columbia (Frank Capra, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1937_the_life_of_emi",
+    "festival": "oscar",
+    "year": 1937,
+    "title_en": "The Life of Emile Zola",
+    "title_cn": "左拉傳",
+    "director": "Warner Bros. (Henry Blanke, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1936_the_great_ziegf",
+    "festival": "oscar",
+    "year": 1936,
+    "title_en": "The Great Ziegfeld",
+    "title_cn": "歌舞大王齊格飛",
+    "director": "Metro-Goldwyn-Mayer (Hunt Stromberg, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1935_mutiny_on_the_b",
+    "festival": "oscar",
+    "year": 1935,
+    "title_en": "Mutiny on the Bounty",
+    "title_cn": "叛艦喋血記",
+    "director": "Metro-Goldwyn-Mayer (Frank Lloyd & Irving Thalberg, producers)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1934_it_happened_one",
+    "festival": "oscar",
+    "year": 1934,
+    "title_en": "It Happened One Night",
+    "title_cn": "一夜風流",
+    "director": "Columbia (Frank Capra & Harry Cohn, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1932_cavalcade_(1933",
+    "festival": "oscar",
+    "year": 1932,
+    "title_en": "Cavalcade",
+    "title_cn": "氣壯山河",
+    "director": "Fox (Frank Lloyd & Winfield Sheehan, producers)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1931_grand_hotel_(19",
+    "festival": "oscar",
+    "year": 1931,
+    "title_en": "Grand Hotel",
+    "title_cn": "大飯店",
+    "director": "Metro-Goldwyn-Mayer (Irving Thalberg, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1930_cimarron_(1931_",
+    "festival": "oscar",
+    "year": 1930,
+    "title_en": "Cimarron",
+    "title_cn": "壯志千秋",
+    "director": "RKO Radio (William LeBaron, producer)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1929_all_quiet_on_th",
+    "festival": "oscar",
+    "year": 1929,
+    "title_en": "All Quiet on the Western Front",
+    "title_cn": "西線無戰事",
+    "director": "Universal (Carl Laemmle)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1928_the_broadway_me",
+    "festival": "oscar",
+    "year": 1928,
+    "title_en": "The Broadway Melody",
+    "title_cn": "百老匯之歌",
+    "director": "Metro-Goldwyn-Mayer (Irving Thalberg & Lawrence Weingarten, producers)",
+    "country": ""
+  },
+  {
+    "id": "oscar_1927_wings_(1927_fil",
+    "festival": "oscar",
+    "year": 1927,
+    "title_en": "Wings",
+    "title_cn": "鐵翼雄風",
+    "director": "Paramount (Lucien Hubbard, Jesse L. Lasky, B. P. Schulberg, & Adolph Zukor, producers)",
+    "country": ""
+  }
+]);
