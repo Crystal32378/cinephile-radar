d1f3f0bc15da46abb7cbed3e6469f50fbcc32ef4
Crystal Chang <crystalys.chang@gmail.com>
2026-06-30 10:49:26 +0800

    Add movie data part 01

---
+++ data-parts/part-01.js
@@ -0,0 +1,679 @@
+// Cinephile Radar data part 01
+window.MOVIE_DATA_PARTS = window.MOVIE_DATA_PARTS || [];
+window.MOVIE_DATA_PARTS.push([
+  {
+    "id": "cannes_2026_fjord_(film)",
+    "festival": "cannes",
+    "year": 2026,
+    "title_en": "Fjord",
+    "title_cn": "峽灣",
+    "director": "Cristian Mungiu",
+    "country": "Romania, Norway, Denmark, France, Sweden"
+  },
+  {
+    "id": "cannes_2025_it_was_just_an_",
+    "festival": "cannes",
+    "year": 2025,
+    "title_en": "It Was Just an Accident",
+    "title_cn": "只是一場意外",
+    "director": "Jafar Panahi",
+    "country": "Iran, France, Luxembourg"
+  },
+  {
+    "id": "cannes_2024_anora",
+    "festival": "cannes",
+    "year": 2024,
+    "title_en": "Anora",
+    "title_cn": "艾諾拉",
+    "director": "Sean Baker",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_2023_anatomy_of_a_fa",
+    "festival": "cannes",
+    "year": 2023,
+    "title_en": "Anatomy of a Fall",
+    "title_cn": "墜惡真相",
+    "director": "Justine Triet",
+    "country": "France"
+  },
+  {
+    "id": "cannes_2022_triangle_of_sad",
+    "festival": "cannes",
+    "year": 2022,
+    "title_en": "Triangle of Sadness",
+    "title_cn": "瘋狂富作用",
+    "director": "Ruben Östlund",
+    "country": "Sweden, Germany, France, United Kingdom"
+  },
+  {
+    "id": "cannes_2021_titane",
+    "festival": "cannes",
+    "year": 2021,
+    "title_en": "Titane",
+    "title_cn": "鈦",
+    "director": "Julia Ducournau",
+    "country": "France, Belgium"
+  },
+  {
+    "id": "cannes_2019_parasite_(2019_",
+    "festival": "cannes",
+    "year": 2019,
+    "title_en": "Parasite §",
+    "title_cn": "寄生上流",
+    "director": "Bong Joon Ho",
+    "country": "South Korea"
+  },
+  {
+    "id": "cannes_2018_shoplifters_(fi",
+    "festival": "cannes",
+    "year": 2018,
+    "title_en": "Shoplifters",
+    "title_cn": "小偷家族",
+    "director": "Hirokazu Kore-eda",
+    "country": "Japan"
+  },
+  {
+    "id": "cannes_2017_the_square_(201",
+    "festival": "cannes",
+    "year": 2017,
+    "title_en": "The Square",
+    "title_cn": "抓狂美術館",
+    "director": "Ruben Östlund",
+    "country": "Sweden, Germany, France, Denmark"
+  },
+  {
+    "id": "cannes_2016_i,_daniel_blake",
+    "festival": "cannes",
+    "year": 2016,
+    "title_en": "I, Daniel Blake",
+    "title_cn": "我是布萊克",
+    "director": "Ken Loach",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "cannes_2015_dheepan",
+    "festival": "cannes",
+    "year": 2015,
+    "title_en": "Dheepan",
+    "title_cn": "流離者之歌",
+    "director": "Jacques Audiard",
+    "country": "France"
+  },
+  {
+    "id": "cannes_2014_winter_sleep_(f",
+    "festival": "cannes",
+    "year": 2014,
+    "title_en": "Winter Sleep",
+    "title_cn": "冬日甦醒",
+    "director": "Nuri Bilge Ceylan",
+    "country": "Turkey, France, Germany"
+  },
+  {
+    "id": "cannes_2013_blue_is_the_war",
+    "festival": "cannes",
+    "year": 2013,
+    "title_en": "Blue Is the Warmest Colour §",
+    "title_cn": "藍色是最溫暖的顏色",
+    "director": "Abdellatif Kechiche",
+    "country": "France, Belgium, Spain"
+  },
+  {
+    "id": "cannes_2012_amour_(2012_fil",
+    "festival": "cannes",
+    "year": 2012,
+    "title_en": "Amour",
+    "title_cn": "愛·慕",
+    "director": "Michael Haneke",
+    "country": "France, Germany, Austria"
+  },
+  {
+    "id": "cannes_2011_the_tree_of_lif",
+    "festival": "cannes",
+    "year": 2011,
+    "title_en": "The Tree of Life",
+    "title_cn": "永生樹",
+    "director": "Terrence Malick",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_2010_uncle_boonmee_w",
+    "festival": "cannes",
+    "year": 2010,
+    "title_en": "Uncle Boonmee Who Can Recall His Past Lives",
+    "title_cn": "波米叔叔的前世今生",
+    "director": "Apichatpong Weerasethakul",
+    "country": "Thailand, France, Germany"
+  },
+  {
+    "id": "cannes_2009_the_white_ribbo",
+    "festival": "cannes",
+    "year": 2009,
+    "title_en": "The White Ribbon",
+    "title_cn": "白色緞帶",
+    "director": "Michael Haneke",
+    "country": "Germany, Austria, France"
+  },
+  {
+    "id": "cannes_2008_the_class_(2008",
+    "festival": "cannes",
+    "year": 2008,
+    "title_en": "The Class §",
+    "title_cn": "我和我的小鬼們",
+    "director": "Laurent Cantet",
+    "country": "France"
+  },
+  {
+    "id": "cannes_2007_4_months,_3_wee",
+    "festival": "cannes",
+    "year": 2007,
+    "title_en": "4 Months, 3 Weeks and 2 Days",
+    "title_cn": "4月3週又2天",
+    "director": "Cristian Mungiu",
+    "country": "Romania"
+  },
+  {
+    "id": "cannes_2006_the_wind_that_s",
+    "festival": "cannes",
+    "year": 2006,
+    "title_en": "The Wind That Shakes the Barley §",
+    "title_cn": "吹動大麥的風",
+    "director": "Ken Loach",
+    "country": "Ireland, United Kingdom, Italy, Germany"
+  },
+  {
+    "id": "cannes_2005_l'enfant_(film)",
+    "festival": "cannes",
+    "year": 2005,
+    "title_en": "L'Enfant",
+    "title_cn": "孩子",
+    "director": "Jean-Pierre & Luc Dardenne",
+    "country": "Belgium, France"
+  },
+  {
+    "id": "cannes_2004_fahrenheit_9/11",
+    "festival": "cannes",
+    "year": 2004,
+    "title_en": "Fahrenheit 9/11",
+    "title_cn": "華氏911",
+    "director": "Michael Moore",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_2003_elephant_(2003_",
+    "festival": "cannes",
+    "year": 2003,
+    "title_en": "Elephant",
+    "title_cn": "大象",
+    "director": "Gus Van Sant",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_2002_the_pianist_(20",
+    "festival": "cannes",
+    "year": 2002,
+    "title_en": "The Pianist",
+    "title_cn": "戰地琴人",
+    "director": "Roman Polanski",
+    "country": "Poland, France, Germany, United Kingdom"
+  },
+  {
+    "id": "cannes_2001_the_son's_room",
+    "festival": "cannes",
+    "year": 2001,
+    "title_en": "The Son's Room",
+    "title_cn": "人間有情天",
+    "director": "Nanni Moretti",
+    "country": "Italy"
+  },
+  {
+    "id": "cannes_2000_dancer_in_the_d",
+    "festival": "cannes",
+    "year": 2000,
+    "title_en": "Dancer in the Dark",
+    "title_cn": "在黑暗中漫舞",
+    "director": "Lars von Trier",
+    "country": "Denmark"
+  },
+  {
+    "id": "cannes_1999_rosetta_(film)",
+    "festival": "cannes",
+    "year": 1999,
+    "title_en": "Rosetta §",
+    "title_cn": "美麗蘿賽塔",
+    "director": "Jean-Pierre & Luc Dardenne",
+    "country": "Belgium"
+  },
+  {
+    "id": "cannes_1998_eternity_and_a_",
+    "festival": "cannes",
+    "year": 1998,
+    "title_en": "Eternity and a Day §",
+    "title_cn": "永遠的一天",
+    "director": "Theo Angelopoulos",
+    "country": "Greece"
+  },
+  {
+    "id": "cannes_1997_the_eel_(film)",
+    "festival": "cannes",
+    "year": 1997,
+    "title_en": "The Eel",
+    "title_cn": "鰻魚",
+    "director": "Shōhei Imamura",
+    "country": "Japan"
+  },
+  {
+    "id": "cannes_1997_taste_of_cherry",
+    "festival": "cannes",
+    "year": 1997,
+    "title_en": "Taste of Cherry",
+    "title_cn": "櫻桃的滋味",
+    "director": "Abbas Kiarostami",
+    "country": "Iran"
+  },
+  {
+    "id": "cannes_1996_secrets_&_lies_",
+    "festival": "cannes",
+    "year": 1996,
+    "title_en": "Secrets & Lies",
+    "title_cn": "秘密與謊言",
+    "director": "Mike Leigh",
+    "country": "France, United Kingdom"
+  },
+  {
+    "id": "cannes_1995_underground_(19",
+    "festival": "cannes",
+    "year": 1995,
+    "title_en": "Underground",
+    "title_cn": "地下社會",
+    "director": "Emir Kusturica",
+    "country": "Yugoslavia"
+  },
+  {
+    "id": "cannes_1994_pulp_fiction",
+    "festival": "cannes",
+    "year": 1994,
+    "title_en": "Pulp Fiction",
+    "title_cn": "黑色追緝令",
+    "director": "Quentin Tarantino",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1993_farewell_my_con",
+    "festival": "cannes",
+    "year": 1993,
+    "title_en": "Farewell My Concubine",
+    "title_cn": "霸王別姬",
+    "director": "Chen Kaige",
+    "country": "Hong Kong"
+  },
+  {
+    "id": "cannes_1993_the_piano",
+    "festival": "cannes",
+    "year": 1993,
+    "title_en": "The Piano",
+    "title_cn": "鋼琴師和她的情人",
+    "director": "Jane Campion",
+    "country": "New Zealand, Australia, France"
+  },
+  {
+    "id": "cannes_1992_the_best_intent",
+    "festival": "cannes",
+    "year": 1992,
+    "title_en": "The Best Intentions",
+    "title_cn": "The Best Intentions",
+    "director": "Bille August",
+    "country": "Denmark, Sweden"
+  },
+  {
+    "id": "cannes_1991_barton_fink",
+    "festival": "cannes",
+    "year": 1991,
+    "title_en": "Barton Fink §",
+    "title_cn": "巴頓芬克",
+    "director": "Joel Coen",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1990_wild_at_heart_(",
+    "festival": "cannes",
+    "year": 1990,
+    "title_en": "Wild at Heart",
+    "title_cn": "我心狂野",
+    "director": "David Lynch",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1989_sex,_lies,_and_",
+    "festival": "cannes",
+    "year": 1989,
+    "title_en": "Sex, Lies, and Videotape",
+    "title_cn": "性、謊言、錄影帶",
+    "director": "Steven Soderbergh",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1988_pelle_the_conqu",
+    "festival": "cannes",
+    "year": 1988,
+    "title_en": "Pelle the Conqueror",
+    "title_cn": "比利小英雄",
+    "director": "Bille August",
+    "country": "Denmark"
+  },
+  {
+    "id": "cannes_1987_under_the_sun_o",
+    "festival": "cannes",
+    "year": 1987,
+    "title_en": "Under the Sun of Satan §",
+    "title_cn": "Under the Sun of Satan §",
+    "director": "Maurice Pialat",
+    "country": "France"
+  },
+  {
+    "id": "cannes_1986_the_mission_(19",
+    "festival": "cannes",
+    "year": 1986,
+    "title_en": "The Mission",
+    "title_cn": "教會",
+    "director": "Roland Joffé",
+    "country": "United Kingdom, United States"
+  },
+  {
+    "id": "cannes_1985_when_father_was",
+    "festival": "cannes",
+    "year": 1985,
+    "title_en": "When Father Was Away on Business §",
+    "title_cn": "爸爸出差時",
+    "director": "Emir Kusturica",
+    "country": "Yugoslavia"
+  },
+  {
+    "id": "cannes_1984_paris,_texas_(f",
+    "festival": "cannes",
+    "year": 1984,
+    "title_en": "Paris, Texas §",
+    "title_cn": "巴黎，德州",
+    "director": "Wim Wenders",
+    "country": "West Germany, France"
+  },
+  {
+    "id": "cannes_1983_the_ballad_of_n",
+    "festival": "cannes",
+    "year": 1983,
+    "title_en": "The Ballad of Narayama",
+    "title_cn": "楢山節考",
+    "director": "Shōhei Imamura",
+    "country": "Japan"
+  },
+  {
+    "id": "cannes_1982_missing_(1982_f",
+    "festival": "cannes",
+    "year": 1982,
+    "title_en": "Missing §",
+    "title_cn": "Missing §",
+    "director": "Costa-Gavras",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1982_yol_(film)",
+    "festival": "cannes",
+    "year": 1982,
+    "title_en": "Yol §",
+    "title_cn": "Yol §",
+    "director": "Yılmaz Güney & Şerif Gören",
+    "country": "Turkey"
+  },
+  {
+    "id": "cannes_1981_man_of_iron",
+    "festival": "cannes",
+    "year": 1981,
+    "title_en": "Man of Iron",
+    "title_cn": "波蘭鐵人",
+    "director": "Andrzej Wajda",
+    "country": "Poland"
+  },
+  {
+    "id": "cannes_1980_all_that_jazz_(",
+    "festival": "cannes",
+    "year": 1980,
+    "title_en": "All That Jazz",
+    "title_cn": "爵士春秋",
+    "director": "Bob Fosse",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1980_kagemusha",
+    "festival": "cannes",
+    "year": 1980,
+    "title_en": "Kagemusha",
+    "title_cn": "影武者",
+    "director": "Akira Kurosawa",
+    "country": "Japan"
+  },
+  {
+    "id": "cannes_1979_apocalypse_now",
+    "festival": "cannes",
+    "year": 1979,
+    "title_en": "Apocalypse Now",
+    "title_cn": "現代啟示錄",
+    "director": "Francis Ford Coppola",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1979_the_tin_drum_(f",
+    "festival": "cannes",
+    "year": 1979,
+    "title_en": "The Tin Drum",
+    "title_cn": "拒絕長大的男生",
+    "director": "Volker Schlöndorff",
+    "country": "West Germany, France"
+  },
+  {
+    "id": "cannes_1978_the_tree_of_woo",
+    "festival": "cannes",
+    "year": 1978,
+    "title_en": "The Tree of Wooden Clogs §",
+    "title_cn": "The Tree of Wooden Clogs §",
+    "director": "Ermanno Olmi",
+    "country": "Italy"
+  },
+  {
+    "id": "cannes_1977_padre_padrone",
+    "festival": "cannes",
+    "year": 1977,
+    "title_en": "Padre Padrone",
+    "title_cn": "我父我主",
+    "director": "Paolo and Vittorio Taviani",
+    "country": "Italy"
+  },
+  {
+    "id": "cannes_1976_taxi_driver",
+    "festival": "cannes",
+    "year": 1976,
+    "title_en": "Taxi Driver",
+    "title_cn": "計程車司機",
+    "director": "Martin Scorsese",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1975_chronicle_of_th",
+    "festival": "cannes",
+    "year": 1975,
+    "title_en": "Chronicle of the Years of Fire",
+    "title_cn": "Chronicle of the Years of Fire",
+    "director": "Mohammed Lakhdar-Hamina",
+    "country": "Algeria"
+  },
+  {
+    "id": "cannes_1974_the_conversatio",
+    "festival": "cannes",
+    "year": 1974,
+    "title_en": "The Conversation",
+    "title_cn": "The Conversation",
+    "director": "Francis Ford Coppola",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1973_the_hireling",
+    "festival": "cannes",
+    "year": 1973,
+    "title_en": "The Hireling",
+    "title_cn": "The Hireling",
+    "director": "Alan Bridges",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "cannes_1973_scarecrow_(1973",
+    "festival": "cannes",
+    "year": 1973,
+    "title_en": "Scarecrow",
+    "title_cn": "Scarecrow",
+    "director": "Jerry Schatzberg",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1972_the_mattei_affa",
+    "festival": "cannes",
+    "year": 1972,
+    "title_en": "The Mattei Affair §",
+    "title_cn": "The Mattei Affair §",
+    "director": "Francesco Rosi",
+    "country": "Italy"
+  },
+  {
+    "id": "cannes_1972_the_working_cla",
+    "festival": "cannes",
+    "year": 1972,
+    "title_en": "The Working Class Goes to Heaven §",
+    "title_cn": "The Working Class Goes to Heaven §",
+    "director": "Elio Petri",
+    "country": "Italy"
+  },
+  {
+    "id": "cannes_1971_the_go-between_",
+    "festival": "cannes",
+    "year": 1971,
+    "title_en": "The Go-Between",
+    "title_cn": "The Go-Between",
+    "director": "Joseph Losey",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "cannes_1970_m*a*s*h_(film)",
+    "festival": "cannes",
+    "year": 1970,
+    "title_en": "M*A*S*H",
+    "title_cn": "外科醫生",
+    "director": "Robert Altman",
+    "country": "United States"
+  },
+  {
+    "id": "cannes_1969_if....",
+    "festival": "cannes",
+    "year": 1969,
+    "title_en": "If....",
+    "title_cn": "如果…",
+    "director": "Lindsay Anderson",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "cannes_1967_blowup",
+    "festival": "cannes",
+    "year": 1967,
+    "title_en": "Blowup",
+    "title_cn": "春光乍現",
+    "director": "Michelangelo Antonioni",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "cannes_1966_the_birds,_the_",
+    "festival": "cannes",
+    "year": 1966,
+    "title_en": "The Birds, the Bees and the Italians",
+    "title_cn": "The Birds, the Bees and the Italians",
+    "director": "Pietro Germi",
+    "country": "Italy"
+  },
+  {
+    "id": "cannes_1966_a_man_and_a_wom",
+    "festival": "cannes",
+    "year": 1966,
+    "title_en": "A Man and a Woman",
+    "title_cn": "男歡女愛",
+    "director": "Claude Lelouch",
+    "country": "France"
+  },
+  {
+    "id": "cannes_1965_the_knack_...an",
+    "festival": "cannes",
+    "year": 1965,
+    "title_en": "The Knack ...and How to Get It",
+    "title_cn": "The Knack ...and How to Get It",
+    "director": "Richard Lester",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "cannes_1964_the_umbrellas_o",
+    "festival": "cannes",
+    "year": 1964,
+    "title_en": "The Umbrellas of Cherbourg",
+    "title_cn": "秋水伊人",
+    "director": "Jacques Demy",
+    "country": "France"
+  },
+  {
+    "id": "cannes_1963_the_leopard_(19",
+    "festival": "cannes",
+    "year": 1963,
+    "title_en": "The Leopard §",
+    "title_cn": "浩氣蓋山河",
+    "director": "Luchino Visconti",
+    "country": "Italy"
+  },
+  {
+    "id": "cannes_1962_o_pagador_de_pr",
+    "festival": "cannes",
+    "year": 1962,
+    "title_en": "O Pagador de Promessas §",
+    "title_cn": "O Pagador de Promessas §",
+    "director": "Anselmo Duarte",
+    "country": "Brazil"
+  },
+  {
+    "id": "cannes_1961_the_long_absenc",
+    "festival": "cannes",
+    "year": 1961,
+    "title_en": "The Long Absence §",
+    "title_cn": "The Long Absence §",
+    "director": "Henri Colpi",
+    "country": "France"
+  },
+  {
+    "id": "cannes_1961_viridiana",
+    "festival": "cannes",
+    "year": 1961,
+    "title_en": "Viridiana §",
+    "title_cn": "維莉蒂安娜",
+    "director": "Luis Buñuel",
+    "country": "Spain"
+  },
+  {
+    "id": "cannes_1960_la_dolce_vita",
+    "festival": "cannes",
+    "year": 1960,
+    "title_en": "La dolce vita §",
+    "title_cn": "生活的甜蜜",
+    "director": "Federico Fellini",
+    "country": "Italy"
+  },
+  {
+    "id": "cannes_1959_black_orpheus",
+    "festival": "cannes",
+    "year": 1959,
+    "title_en": "Black Orpheus §",
+    "title_cn": "Black Orpheus §",
+    "director": "Marcel Camus",
+    "country": "France, Brazil"
+  }
+]);
