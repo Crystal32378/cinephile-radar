72a083b5088aa2db1bab16e08828d380c899e049
Crystal Chang <crystalys.chang@gmail.com>
2026-06-30 10:59:46 +0800

    Add movie data part 05

---
+++ data-parts/part-05.js
@@ -0,0 +1,679 @@
+// Cinephile Radar data part 05
+window.MOVIE_DATA_PARTS = window.MOVIE_DATA_PARTS || [];
+window.MOVIE_DATA_PARTS.push([
+  {
+    "id": "berlin_1951_without_leaving",
+    "festival": "berlin",
+    "year": 1951,
+    "title_en": "Without Leaving an Address (Comedy)",
+    "title_cn": "Without Leaving an Address (Comedy)",
+    "director": "Jean-Paul Le Chanois",
+    "country": "France"
+  },
+  {
+    "id": "berlin_1951_in_beaver_valle",
+    "festival": "berlin",
+    "year": 1951,
+    "title_en": "In Beaver Valley (Documentary)",
+    "title_cn": "In Beaver Valley (Documentary)",
+    "director": "James Algar",
+    "country": "United States"
+  },
+  {
+    "id": "berlin_1951_justice_is_done",
+    "festival": "berlin",
+    "year": 1951,
+    "title_en": "Justice Is Done (Thriller & Adventure)",
+    "title_cn": "Justice Is Done (Thriller & Adventure)",
+    "director": "André Cayatte",
+    "country": "France"
+  },
+  {
+    "id": "berlin_1951_cinderella_(195",
+    "festival": "berlin",
+    "year": 1951,
+    "title_en": "Cinderella (Music Film)",
+    "title_cn": "仙履奇緣",
+    "director": "Wilfred Jackson",
+    "country": "United States"
+  },
+  {
+    "id": "oscar_2025_one_battle_afte",
+    "festival": "oscar",
+    "year": 2025,
+    "title_en": "One Battle After Another",
+    "title_cn": "一戰再戰",
+    "director": "Adam Somner, Sara Murphy, and Paul Thomas Anderson",
+    "country": ""
+  },
+  {
+    "id": "oscar_2024_anora",
+    "festival": "oscar",
+    "year": 2024,
+    "title_en": "Anora",
+    "title_cn": "艾諾拉",
+    "director": "Alex Coco, Samantha Quan, and Sean Baker",
+    "country": ""
+  },
+  {
+    "id": "oscar_2023_oppenheimer_(fi",
+    "festival": "oscar",
+    "year": 2023,
+    "title_en": "Oppenheimer",
+    "title_cn": "奧本海默",
+    "director": "Emma Thomas, Charles Roven, and Christopher Nolan",
+    "country": ""
+  },
+  {
+    "id": "oscar_2022_everything_ever",
+    "festival": "oscar",
+    "year": 2022,
+    "title_en": "Everything Everywhere All at Once",
+    "title_cn": "媽的多重宇宙",
+    "director": "Daniel Kwan, Daniel Scheinert, and Jonathan Wang",
+    "country": ""
+  },
+  {
+    "id": "oscar_2021_coda_(2021_film",
+    "festival": "oscar",
+    "year": 2021,
+    "title_en": "CODA",
+    "title_cn": "樂動心旋律",
+    "director": "Philippe Rousselet, Fabrice Gianfermi, and Patrick Wachsberger",
+    "country": ""
+  },
+  {
+    "id": "oscar_2020_nomadland",
+    "festival": "oscar",
+    "year": 2020,
+    "title_en": "Nomadland",
+    "title_cn": "游牧人生",
+    "director": "Frances McDormand, Peter Spears, Mollye Asher, Dan Janvey, and Chloé Zhao",
+    "country": ""
+  },
+  {
+    "id": "oscar_2019_parasite_(2019_",
+    "festival": "oscar",
+    "year": 2019,
+    "title_en": "Parasite",
+    "title_cn": "寄生上流",
+    "director": "Kwak Sin-ae and Bong Joon Ho",
+    "country": ""
+  },
+  {
+    "id": "oscar_2018_green_book_(fil",
+    "festival": "oscar",
+    "year": 2018,
+    "title_en": "Green Book",
+    "title_cn": "幸福綠皮書",
+    "director": "Jim Burke, Charles B. Wessler, Brian Currie, Peter Farrelly, and Nick Vallelonga",
+    "country": ""
+  },
+  {
+    "id": "oscar_2017_the_shape_of_wa",
+    "festival": "oscar",
+    "year": 2017,
+    "title_en": "The Shape of Water",
+    "title_cn": "水底情深",
+    "director": "Guillermo del Toro and J. Miles Dale",
+    "country": ""
+  },
+  {
+    "id": "oscar_2016_moonlight_(2016",
+    "festival": "oscar",
+    "year": 2016,
+    "title_en": "Moonlight",
+    "title_cn": "月光下的藍色男孩",
+    "director": "Adele Romanski, Dede Gardner, and Jeremy Kleiner",
+    "country": ""
+  },
+  {
+    "id": "oscar_2015_spotlight_(film",
+    "festival": "oscar",
+    "year": 2015,
+    "title_en": "Spotlight",
+    "title_cn": "驚爆焦點",
+    "director": "Blye Pagon Faust, Steve Golin, Nicole Rocklin, and Michael Sugar",
+    "country": ""
+  },
+  {
+    "id": "oscar_2014_birdman_(film)",
+    "festival": "oscar",
+    "year": 2014,
+    "title_en": "Birdman or (The Unexpected Virtue of Ignorance)",
+    "title_cn": "鳥人",
+    "director": "Alejandro González Iñárritu, John Lesher, and James W. Skotchdopole",
+    "country": ""
+  },
+  {
+    "id": "oscar_2013_12_years_a_slav",
+    "festival": "oscar",
+    "year": 2013,
+    "title_en": "12 Years a Slave",
+    "title_cn": "自由之心",
+    "director": "Brad Pitt, Dede Gardner, Jeremy Kleiner, Steve McQueen, and Anthony Katagas",
+    "country": ""
+  },
+  {
+    "id": "oscar_2012_argo_(2012_film",
+    "festival": "oscar",
+    "year": 2012,
+    "title_en": "Argo",
+    "title_cn": "亞果出任務",
+    "director": "Grant Heslov, Ben Affleck, and George Clooney",
+    "country": ""
+  },
+  {
+    "id": "oscar_2011_the_artist_(fil",
+    "festival": "oscar",
+    "year": 2011,
+    "title_en": "The Artist",
+    "title_cn": "大藝術家",
+    "director": "Thomas Langmann",
+    "country": ""
+  },
+  {
+    "id": "oscar_2010_the_king's_spee",
+    "festival": "oscar",
+    "year": 2010,
+    "title_en": "The King's Speech",
+    "title_cn": "王者之聲：宣戰時刻",
+    "director": "Iain Canning, Emile Sherman, and Gareth Unwin",
+    "country": ""
+  },
+  {
+    "id": "oscar_2009_the_hurt_locker",
+    "festival": "oscar",
+    "year": 2009,
+    "title_en": "The Hurt Locker",
+    "title_cn": "危機倒數",
+    "director": "Kathryn Bigelow, Mark Boal, Nicolas Chartier, and Greg Shapiro",
+    "country": ""
+  },
+  {
+    "id": "oscar_2008_slumdog_million",
+    "festival": "oscar",
+    "year": 2008,
+    "title_en": "Slumdog Millionaire",
+    "title_cn": "貧民百萬富翁",
+    "director": "Christian Colson",
+    "country": ""
+  },
+  {
+    "id": "oscar_2007_no_country_for_",
+    "festival": "oscar",
+    "year": 2007,
+    "title_en": "No Country for Old Men",
+    "title_cn": "險路勿近",
+    "director": "Scott Rudin, Joel Coen, and Ethan Coen",
+    "country": ""
+  },
+  {
+    "id": "oscar_2006_the_departed",
+    "festival": "oscar",
+    "year": 2006,
+    "title_en": "The Departed",
+    "title_cn": "神鬼無間",
+    "director": "Graham King",
+    "country": ""
+  },
+  {
+    "id": "oscar_2005_crash_(2004_fil",
+    "festival": "oscar",
+    "year": 2005,
+    "title_en": "Crash",
+    "title_cn": "衝擊效應",
+    "director": "Paul Haggis and Cathy Schulman",
+    "country": ""
+  },
+  {
+    "id": "oscar_2004_million_dollar_",
+    "festival": "oscar",
+    "year": 2004,
+    "title_en": "Million Dollar Baby",
+    "title_cn": "登峰造擊",
+    "director": "Clint Eastwood, Albert S. Ruddy, and Tom Rosenberg",
+    "country": ""
+  },
+  {
+    "id": "oscar_2003_the_lord_of_the",
+    "festival": "oscar",
+    "year": 2003,
+    "title_en": "The Lord of the Rings: The Return of the King",
+    "title_cn": "魔戒三部曲：王者再臨",
+    "director": "Barrie M. Osborne, Peter Jackson, and Fran Walsh",
+    "country": ""
+  },
+  {
+    "id": "oscar_2002_chicago_(2002_f",
+    "festival": "oscar",
+    "year": 2002,
+    "title_en": "Chicago",
+    "title_cn": "芝加哥",
+    "director": "Martin Richards",
+    "country": ""
+  },
+  {
+    "id": "oscar_2001_a_beautiful_min",
+    "festival": "oscar",
+    "year": 2001,
+    "title_en": "A Beautiful Mind",
+    "title_cn": "美麗境界",
+    "director": "Brian Grazer and Ron Howard",
+    "country": ""
+  },
+  {
+    "id": "oscar_2000_gladiator_(2000",
+    "festival": "oscar",
+    "year": 2000,
+    "title_en": "Gladiator",
+    "title_cn": "神鬼戰士",
+    "director": "Douglas Wick, David Franzoni, and Branko Lustig",
+    "country": ""
+  },
+  {
+    "id": "oscar_1999_american_beauty",
+    "festival": "oscar",
+    "year": 1999,
+    "title_en": "American Beauty",
+    "title_cn": "美國心玫瑰情",
+    "director": "Bruce Cohen and Dan Jinks",
+    "country": ""
+  },
+  {
+    "id": "oscar_1998_shakespeare_in_",
+    "festival": "oscar",
+    "year": 1998,
+    "title_en": "Shakespeare in Love",
+    "title_cn": "莎翁情史",
+    "director": "David Parfitt, Donna Gigliotti, Harvey Weinstein, Edward Zwick, and Marc Norman",
+    "country": ""
+  },
+  {
+    "id": "oscar_1997_titanic_(1997_f",
+    "festival": "oscar",
+    "year": 1997,
+    "title_en": "Titanic",
+    "title_cn": "鐵達尼號",
+    "director": "James Cameron and Jon Landau",
+    "country": ""
+  },
+  {
+    "id": "oscar_1996_the_english_pat",
+    "festival": "oscar",
+    "year": 1996,
+    "title_en": "The English Patient",
+    "title_cn": "英倫情人",
+    "director": "Saul Zaentz",
+    "country": ""
+  },
+  {
+    "id": "oscar_1995_braveheart",
+    "festival": "oscar",
+    "year": 1995,
+    "title_en": "Braveheart",
+    "title_cn": "梅爾吉勃遜之英雄本色",
+    "director": "Mel Gibson, Alan Ladd Jr., and Bruce Davey",
+    "country": ""
+  },
+  {
+    "id": "oscar_1994_forrest_gump",
+    "festival": "oscar",
+    "year": 1994,
+    "title_en": "Forrest Gump",
+    "title_cn": "阿甘正傳",
+    "director": "Wendy Finerman, Steve Tisch, and Steve Starkey",
+    "country": ""
+  },
+  {
+    "id": "oscar_1993_schindler's_lis",
+    "festival": "oscar",
+    "year": 1993,
+    "title_en": "Schindler's List",
+    "title_cn": "辛德勒的名單",
+    "director": "Steven Spielberg, Gerald R. Molen, and Branko Lustig",
+    "country": ""
+  },
+  {
+    "id": "oscar_1992_unforgiven",
+    "festival": "oscar",
+    "year": 1992,
+    "title_en": "Unforgiven",
+    "title_cn": "殺無赦",
+    "director": "Clint Eastwood",
+    "country": ""
+  },
+  {
+    "id": "oscar_1991_the_silence_of_",
+    "festival": "oscar",
+    "year": 1991,
+    "title_en": "The Silence of the Lambs",
+    "title_cn": "沉默的羔羊",
+    "director": "Edward Saxon, Kenneth Utt, and Ron Bozman",
+    "country": ""
+  },
+  {
+    "id": "oscar_1990_dances_with_wol",
+    "festival": "oscar",
+    "year": 1990,
+    "title_en": "Dances With Wolves",
+    "title_cn": "與狼共舞",
+    "director": "Jim Wilson and Kevin Costner",
+    "country": ""
+  },
+  {
+    "id": "oscar_1989_driving_miss_da",
+    "festival": "oscar",
+    "year": 1989,
+    "title_en": "Driving Miss Daisy",
+    "title_cn": "溫馨接送情",
+    "director": "Richard D. Zanuck and Lili Fini Zanuck",
+    "country": ""
+  },
+  {
+    "id": "oscar_1988_rain_man",
+    "festival": "oscar",
+    "year": 1988,
+    "title_en": "Rain Man",
+    "title_cn": "雨人",
+    "director": "Mark Johnson",
+    "country": ""
+  },
+  {
+    "id": "oscar_1987_the_last_empero",
+    "festival": "oscar",
+    "year": 1987,
+    "title_en": "The Last Emperor",
+    "title_cn": "末代皇帝",
+    "director": "Jeremy Thomas",
+    "country": ""
+  },
+  {
+    "id": "oscar_1986_platoon_(film)",
+    "festival": "oscar",
+    "year": 1986,
+    "title_en": "Platoon",
+    "title_cn": "前進高棉",
+    "director": "Arnold Kopelson",
+    "country": ""
+  },
+  {
+    "id": "oscar_1985_out_of_africa_(",
+    "festival": "oscar",
+    "year": 1985,
+    "title_en": "Out of Africa",
+    "title_cn": "遠離非洲",
+    "director": "Sydney Pollack",
+    "country": ""
+  },
+  {
+    "id": "oscar_1984_amadeus_(film)",
+    "festival": "oscar",
+    "year": 1984,
+    "title_en": "Amadeus",
+    "title_cn": "阿瑪迪斯",
+    "director": "Saul Zaentz",
+    "country": ""
+  },
+  {
+    "id": "oscar_1983_terms_of_endear",
+    "festival": "oscar",
+    "year": 1983,
+    "title_en": "Terms of Endearment",
+    "title_cn": "親密關係",
+    "director": "James L. Brooks",
+    "country": ""
+  },
+  {
+    "id": "oscar_1982_gandhi_(film)",
+    "festival": "oscar",
+    "year": 1982,
+    "title_en": "Gandhi",
+    "title_cn": "甘地",
+    "director": "Richard Attenborough",
+    "country": ""
+  },
+  {
+    "id": "oscar_1981_chariots_of_fir",
+    "festival": "oscar",
+    "year": 1981,
+    "title_en": "Chariots of Fire",
+    "title_cn": "火戰車",
+    "director": "David Puttnam",
+    "country": ""
+  },
+  {
+    "id": "oscar_1980_ordinary_people",
+    "festival": "oscar",
+    "year": 1980,
+    "title_en": "Ordinary People",
+    "title_cn": "凡夫俗子",
+    "director": "Ronald L. Schwary",
+    "country": ""
+  },
+  {
+    "id": "oscar_1979_kramer_vs._kram",
+    "festival": "oscar",
+    "year": 1979,
+    "title_en": "Kramer vs. Kramer",
+    "title_cn": "克拉瑪對克拉瑪",
+    "director": "Stanley R. Jaffe",
+    "country": ""
+  },
+  {
+    "id": "oscar_1978_the_deer_hunter",
+    "festival": "oscar",
+    "year": 1978,
+    "title_en": "The Deer Hunter",
+    "title_cn": "越戰獵鹿人",
+    "director": "Barry Spikings, Michael Deeley, Michael Cimino, and John Peverall",
+    "country": ""
+  },
+  {
+    "id": "oscar_1977_annie_hall",
+    "festival": "oscar",
+    "year": 1977,
+    "title_en": "Annie Hall",
+    "title_cn": "安妮霍爾",
+    "director": "Charles H. Joffe",
+    "country": ""
+  },
+  {
+    "id": "oscar_1976_rocky",
+    "festival": "oscar",
+    "year": 1976,
+    "title_en": "Rocky",
+    "title_cn": "洛基",
+    "director": "Irwin Winkler and Robert Chartoff",
+    "country": ""
+  },
+  {
+    "id": "oscar_1975_one_flew_over_t",
+    "festival": "oscar",
+    "year": 1975,
+    "title_en": "One Flew Over the Cuckoo's Nest",
+    "title_cn": "飛越杜鵑窩",
+    "director": "Michael Douglas and Saul Zaentz",
+    "country": ""
+  },
+  {
+    "id": "oscar_1974_the_godfather_p",
+    "festival": "oscar",
+    "year": 1974,
+    "title_en": "The Godfather Part II",
+    "title_cn": "教父續集",
+    "director": "Francis Ford Coppola, Gray Frederickson, and Fred Roos",
+    "country": ""
+  },
+  {
+    "id": "oscar_1973_the_sting",
+    "festival": "oscar",
+    "year": 1973,
+    "title_en": "The Sting",
+    "title_cn": "刺激",
+    "director": "Tony Bill, Michael Phillips, and Julia Phillips",
+    "country": ""
+  },
+  {
+    "id": "oscar_1972_the_godfather",
+    "festival": "oscar",
+    "year": 1972,
+    "title_en": "The Godfather",
+    "title_cn": "教父",
+    "director": "Albert S. Ruddy",
+    "country": ""
+  },
+  {
+    "id": "oscar_1971_the_french_conn",
+    "festival": "oscar",
+    "year": 1971,
+    "title_en": "The French Connection",
+    "title_cn": "霹靂神探",
+    "director": "Philip D'Antoni",
+    "country": ""
+  },
+  {
+    "id": "oscar_1970_patton_(film)",
+    "festival": "oscar",
+    "year": 1970,
+    "title_en": "Patton",
+    "title_cn": "巴頓將軍",
+    "director": "Frank McCarthy",
+    "country": ""
+  },
+  {
+    "id": "oscar_1969_midnight_cowboy",
+    "festival": "oscar",
+    "year": 1969,
+    "title_en": "Midnight Cowboy",
+    "title_cn": "午夜牛郎",
+    "director": "Jerome Hellman",
+    "country": ""
+  },
+  {
+    "id": "oscar_1968_oliver!_(film)",
+    "festival": "oscar",
+    "year": 1968,
+    "title_en": "Oliver!",
+    "title_cn": "孤雛淚",
+    "director": "John Woolf",
+    "country": ""
+  },
+  {
+    "id": "oscar_1967_in_the_heat_of_",
+    "festival": "oscar",
+    "year": 1967,
+    "title_en": "In the Heat of the Night",
+    "title_cn": "惡夜追緝令",
+    "director": "Walter Mirisch",
+    "country": ""
+  },
+  {
+    "id": "oscar_1966_a_man_for_all_s",
+    "festival": "oscar",
+    "year": 1966,
+    "title_en": "A Man for All Seasons",
+    "title_cn": "良相佐國",
+    "director": "Fred Zinnemann",
+    "country": ""
+  },
+  {
+    "id": "oscar_1965_the_sound_of_mu",
+    "festival": "oscar",
+    "year": 1965,
+    "title_en": "The Sound of Music",
+    "title_cn": "真善美",
+    "director": "Robert Wise",
+    "country": ""
+  },
+  {
+    "id": "oscar_1964_my_fair_lady_(f",
+    "festival": "oscar",
+    "year": 1964,
+    "title_en": "My Fair Lady",
+    "title_cn": "窈窕淑女",
+    "director": "Jack L. Warner",
+    "country": ""
+  },
+  {
+    "id": "oscar_1963_tom_jones_(1963",
+    "festival": "oscar",
+    "year": 1963,
+    "title_en": "Tom Jones",
+    "title_cn": "風流劍客走天涯",
+    "director": "Tony Richardson",
+    "country": ""
+  },
+  {
+    "id": "oscar_1962_lawrence_of_ara",
+    "festival": "oscar",
+    "year": 1962,
+    "title_en": "Lawrence of Arabia",
+    "title_cn": "阿拉伯的勞倫斯",
+    "director": "Sam Spiegel",
+    "country": ""
+  },
+  {
+    "id": "oscar_1961_west_side_story",
+    "festival": "oscar",
+    "year": 1961,
+    "title_en": "West Side Story",
+    "title_cn": "西城故事",
+    "director": "Robert Wise",
+    "country": ""
+  },
+  {
+    "id": "oscar_1960_the_apartment",
+    "festival": "oscar",
+    "year": 1960,
+    "title_en": "The Apartment",
+    "title_cn": "公寓春光",
+    "director": "Billy Wilder",
+    "country": ""
+  },
+  {
+    "id": "oscar_1959_ben-hur_(1959_f",
+    "festival": "oscar",
+    "year": 1959,
+    "title_en": "Ben-Hur",
+    "title_cn": "賓漢",
+    "director": "Sam Zimbalist",
+    "country": ""
+  },
+  {
+    "id": "oscar_1958_gigi_(1958_film",
+    "festival": "oscar",
+    "year": 1958,
+    "title_en": "Gigi",
+    "title_cn": "金粉世界",
+    "director": "Arthur Freed",
+    "country": ""
+  },
+  {
+    "id": "oscar_1957_the_bridge_on_t",
+    "festival": "oscar",
+    "year": 1957,
+    "title_en": "The Bridge on the River Kwai",
+    "title_cn": "桂河大橋",
+    "director": "Sam Spiegel",
+    "country": ""
+  },
+  {
+    "id": "oscar_1956_around_the_worl",
+    "festival": "oscar",
+    "year": 1956,
+    "title_en": "Around the World in 80 Days",
+    "title_cn": "環遊世界八十天",
+    "director": "Michael Todd",
+    "country": ""
+  },
+  {
+    "id": "oscar_1955_marty_(film)",
+    "festival": "oscar",
+    "year": 1955,
+    "title_en": "Marty",
+    "title_cn": "馬蒂",
+    "director": "Harold Hecht",
+    "country": ""
+  }
+]);
