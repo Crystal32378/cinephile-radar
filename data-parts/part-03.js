768bf0ed26d297e91d2d00acd49612a10c52113a
Crystal Chang <crystalys.chang@gmail.com>
2026-06-30 10:54:00 +0800

    Add movie data part 03

---
+++ data-parts/part-03.js
@@ -0,0 +1,679 @@
+// Cinephile Radar data part 03
+window.MOVIE_DATA_PARTS = window.MOVIE_DATA_PARTS || [];
+window.MOVIE_DATA_PARTS.push([
+  {
+    "id": "venice_2001_monsoon_wedding",
+    "festival": "venice",
+    "year": 2001,
+    "title_en": "Monsoon Wedding",
+    "title_cn": "Monsoon Wedding",
+    "director": "Mira Nair",
+    "country": "India"
+  },
+  {
+    "id": "venice_2001_éric_rohmer",
+    "festival": "venice",
+    "year": 2001,
+    "title_en": "Éric Rohmer",
+    "title_cn": "艾力·侯麥",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_2000_the_circle_(200",
+    "festival": "venice",
+    "year": 2000,
+    "title_en": "The Circle",
+    "title_cn": "生命的圓圈",
+    "director": "Jafar Panahi",
+    "country": "Iran"
+  },
+  {
+    "id": "venice_2000_clint_eastwood",
+    "festival": "venice",
+    "year": 2000,
+    "title_en": "Clint Eastwood",
+    "title_cn": "克林·伊斯威特",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1999_not_one_less",
+    "festival": "venice",
+    "year": 1999,
+    "title_en": "Not One Less",
+    "title_cn": "一個都不能少",
+    "director": "Zhang Yimou",
+    "country": "China"
+  },
+  {
+    "id": "venice_1999_jerry_lewis",
+    "festival": "venice",
+    "year": 1999,
+    "title_en": "Jerry Lewis",
+    "title_cn": "傑利·路易斯",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1998_the_way_we_laug",
+    "festival": "venice",
+    "year": 1998,
+    "title_en": "The Way We Laughed",
+    "title_cn": "The Way We Laughed",
+    "director": "Gianni Amelio",
+    "country": "Italy"
+  },
+  {
+    "id": "venice_1998_warren_beatty",
+    "festival": "venice",
+    "year": 1998,
+    "title_en": "Warren Beatty, Sophia Loren, and Andrzej Wajda",
+    "title_cn": "華倫·比提",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1997_hana-bi",
+    "festival": "venice",
+    "year": 1997,
+    "title_en": "Hana-bi",
+    "title_cn": "花火",
+    "director": "Takeshi Kitano",
+    "country": "Japan"
+  },
+  {
+    "id": "venice_1997_gérard_depardie",
+    "festival": "venice",
+    "year": 1997,
+    "title_en": "Gérard Depardieu, Stanley Kubrick, and Alida Valli",
+    "title_cn": "傑哈·德巴狄厄",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1996_michael_collins",
+    "festival": "venice",
+    "year": 1996,
+    "title_en": "Michael Collins",
+    "title_cn": "豪情本色",
+    "director": "Neil Jordan",
+    "country": "Ireland, United Kingdom"
+  },
+  {
+    "id": "venice_1996_robert_altman",
+    "festival": "venice",
+    "year": 1996,
+    "title_en": "Robert Altman, Vittorio Gassman, Dustin Hoffman, and Michèle Morgan",
+    "title_cn": "勞勃·阿特曼",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1995_cyclo_(film)",
+    "festival": "venice",
+    "year": 1995,
+    "title_en": "Cyclo",
+    "title_cn": "Cyclo",
+    "director": "Anh Hung Tran",
+    "country": "Vietnam, France"
+  },
+  {
+    "id": "venice_1995_woody_allen",
+    "festival": "venice",
+    "year": 1995,
+    "title_en": "Woody Allen, Monica Vitti, Martin Scorsese, Alberto Sordi, Ennio Morricone, Giuseppe De Santis, Goffredo Lombardo, and Alain Resnais",
+    "title_cn": "伍迪·艾倫",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1994_before_the_rain",
+    "festival": "venice",
+    "year": 1994,
+    "title_en": "Before the Rain",
+    "title_cn": "Before the Rain",
+    "director": "Milčo Mančevski",
+    "country": "Macedonia"
+  },
+  {
+    "id": "venice_1994_vive_l'amour",
+    "festival": "venice",
+    "year": 1994,
+    "title_en": "Vive L'Amour",
+    "title_cn": "愛情萬歲",
+    "director": "Tsai Ming-liang",
+    "country": "Taiwan"
+  },
+  {
+    "id": "venice_1994_al_pacino",
+    "festival": "venice",
+    "year": 1994,
+    "title_en": "Al Pacino, Suso Cecchi d'Amico, and Ken Loach",
+    "title_cn": "艾爾·帕西諾",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1993_short_cuts",
+    "festival": "venice",
+    "year": 1993,
+    "title_en": "Short Cuts",
+    "title_cn": "銀色·性·男女",
+    "director": "Robert Altman",
+    "country": "United States"
+  },
+  {
+    "id": "venice_1993_three_colours:_",
+    "festival": "venice",
+    "year": 1993,
+    "title_en": "Three Colours: Blue",
+    "title_cn": "藍色情挑",
+    "director": "Krzysztof Kieślowski",
+    "country": "France, Poland"
+  },
+  {
+    "id": "venice_1993_steven_spielber",
+    "festival": "venice",
+    "year": 1993,
+    "title_en": "Steven Spielberg, Robert De Niro, Roman Polanski, and Claudia Cardinale",
+    "title_cn": "史蒂芬·史匹柏",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1992_the_story_of_qi",
+    "festival": "venice",
+    "year": 1992,
+    "title_en": "The Story of Qiu Ju",
+    "title_cn": "秋菊打官司",
+    "director": "Zhang Yimou",
+    "country": "China"
+  },
+  {
+    "id": "venice_1992_jeanne_moreau",
+    "festival": "venice",
+    "year": 1992,
+    "title_en": "Jeanne Moreau, Francis Ford Coppola, and Paolo Villaggio",
+    "title_cn": "珍妮·摩露",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1991_close_to_eden",
+    "festival": "venice",
+    "year": 1991,
+    "title_en": "Close to Eden",
+    "title_cn": "蒙古精神",
+    "director": "Nikita Mikhalkov",
+    "country": "Soviet Union"
+  },
+  {
+    "id": "venice_1991_mario_monicelli",
+    "festival": "venice",
+    "year": 1991,
+    "title_en": "Mario Monicelli and Gian Maria Volonté",
+    "title_cn": "馬里奧·莫尼切利",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1990_rosencrantz_&_g",
+    "festival": "venice",
+    "year": 1990,
+    "title_en": "Rosencrantz & Guildenstern Are Dead",
+    "title_cn": "Rosencrantz & Guildenstern Are Dead",
+    "director": "Tom Stoppard",
+    "country": "United Kingdom, United States"
+  },
+  {
+    "id": "venice_1990_marcello_mastro",
+    "festival": "venice",
+    "year": 1990,
+    "title_en": "Marcello Mastroianni and Miklós Jancsó",
+    "title_cn": "馬切洛·馬斯楚安尼",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1989_a_city_of_sadne",
+    "festival": "venice",
+    "year": 1989,
+    "title_en": "A City of Sadness",
+    "title_cn": "悲情城市",
+    "director": "Hou Hsiao-hsien",
+    "country": "Taiwan"
+  },
+  {
+    "id": "venice_1989_robert_bresson",
+    "festival": "venice",
+    "year": 1989,
+    "title_en": "Robert Bresson",
+    "title_cn": "羅伯特·布列松",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1988_the_legend_of_t",
+    "festival": "venice",
+    "year": 1988,
+    "title_en": "The Legend of the Holy Drinker",
+    "title_cn": "The Legend of the Holy Drinker",
+    "director": "Ermanno Olmi",
+    "country": "Italy, France"
+  },
+  {
+    "id": "venice_1988_joris_ivens",
+    "festival": "venice",
+    "year": 1988,
+    "title_en": "Joris Ivens",
+    "title_cn": "尤里斯·伊文思",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1987_au_revoir_les_e",
+    "festival": "venice",
+    "year": 1987,
+    "title_en": "Au revoir les enfants",
+    "title_cn": "Au revoir les enfants",
+    "director": "Louis Malle",
+    "country": "France, West Germany"
+  },
+  {
+    "id": "venice_1987_luigi_comencini",
+    "festival": "venice",
+    "year": 1987,
+    "title_en": "Luigi Comencini and Joseph L. Mankiewicz",
+    "title_cn": "路易吉·科門西尼",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1986_the_green_ray_(",
+    "festival": "venice",
+    "year": 1986,
+    "title_en": "The Green Ray",
+    "title_cn": "綠光",
+    "director": "Éric Rohmer",
+    "country": "France"
+  },
+  {
+    "id": "venice_1986_paolo_and_vitto",
+    "festival": "venice",
+    "year": 1986,
+    "title_en": "Paolo Taviani and Vittorio Taviani",
+    "title_cn": "塔維亞尼兄弟",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1985_vagabond_(1985_",
+    "festival": "venice",
+    "year": 1985,
+    "title_en": "Vagabond",
+    "title_cn": "Vagabond",
+    "director": "Agnès Varda",
+    "country": "France"
+  },
+  {
+    "id": "venice_1985_manoel_de_olive",
+    "festival": "venice",
+    "year": 1985,
+    "title_en": "Manoel de Oliveira, John Huston, and Federico Fellini",
+    "title_cn": "曼諾·迪·奧利維拉",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1984_a_year_of_the_q",
+    "festival": "venice",
+    "year": 1984,
+    "title_en": "A Year of the Quiet Sun",
+    "title_cn": "A Year of the Quiet Sun",
+    "director": "Krzysztof Zanussi",
+    "country": "Poland"
+  },
+  {
+    "id": "venice_1983_first_name:_car",
+    "festival": "venice",
+    "year": 1983,
+    "title_en": "First Name: Carmen",
+    "title_cn": "芳名卡門",
+    "director": "Jean-Luc Godard",
+    "country": "France"
+  },
+  {
+    "id": "venice_1983_michelangelo_an",
+    "festival": "venice",
+    "year": 1983,
+    "title_en": "Michelangelo Antonioni",
+    "title_cn": "米開朗基羅·安東尼奧尼",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1982_the_state_of_th",
+    "festival": "venice",
+    "year": 1982,
+    "title_en": "The State of Things",
+    "title_cn": "事物的狀態",
+    "director": "Wim Wenders",
+    "country": "West Germany"
+  },
+  {
+    "id": "venice_1982_alessandro_blas",
+    "festival": "venice",
+    "year": 1982,
+    "title_en": "Alessandro Blasetti, Luis Buñuel, Frank Capra, George Cukor, Jean-Luc Godard, Sergei Yutkevich, Alexander Kluge, Akira Kurosawa, Michael Powell, Satyajit Ray, King Vidor, and Cesare Zavattini",
+    "title_cn": "Alessandro Blasetti, Luis Buñuel, Frank Capra, George Cukor, Jean-Luc Godard, Sergei Yutkevich, Alexander Kluge, Akira Kurosawa, Michael Powell, Satyajit Ray, King Vidor, and Cesare Zavattini",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1981_marianne_and_ju",
+    "festival": "venice",
+    "year": 1981,
+    "title_en": "Marianne and Juliane",
+    "title_cn": "Marianne and Juliane",
+    "director": "Margarethe von Trotta",
+    "country": "West Germany"
+  },
+  {
+    "id": "venice_1980_atlantic_city_(",
+    "festival": "venice",
+    "year": 1980,
+    "title_en": "Atlantic City",
+    "title_cn": "大西洋城",
+    "director": "Louis Malle",
+    "country": "Canada, France"
+  },
+  {
+    "id": "venice_1980_gloria_(1980_fi",
+    "festival": "venice",
+    "year": 1980,
+    "title_en": "Gloria",
+    "title_cn": "Gloria",
+    "director": "John Cassavetes",
+    "country": "United States"
+  },
+  {
+    "id": "venice_1972_charlie_chaplin",
+    "festival": "venice",
+    "year": 1972,
+    "title_en": "Charlie Chaplin, Anatoli Golovnya and Billy Wilder",
+    "title_cn": "查理·卓別林",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1971_ingmar_bergman",
+    "festival": "venice",
+    "year": 1971,
+    "title_en": "Ingmar Bergman, Marcel Carné, and John Ford",
+    "title_cn": "英格瑪·柏格曼",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1970_orson_welles",
+    "festival": "venice",
+    "year": 1970,
+    "title_en": "Orson Welles",
+    "title_cn": "奧森·威爾斯",
+    "director": "",
+    "country": ""
+  },
+  {
+    "id": "venice_1968_artists_under_t",
+    "festival": "venice",
+    "year": 1968,
+    "title_en": "Artists Under the Big Top: Perplexed",
+    "title_cn": "Artists Under the Big Top: Perplexed",
+    "director": "Alexander Kluge",
+    "country": "West Germany"
+  },
+  {
+    "id": "venice_1967_belle_de_jour_(",
+    "festival": "venice",
+    "year": 1967,
+    "title_en": "Belle de Jour",
+    "title_cn": "Belle de Jour",
+    "director": "Luis Buñuel",
+    "country": "France"
+  },
+  {
+    "id": "venice_1966_the_battle_of_a",
+    "festival": "venice",
+    "year": 1966,
+    "title_en": "The Battle of Algiers",
+    "title_cn": "阿爾及爾之戰",
+    "director": "Gillo Pontecorvo",
+    "country": "Algeria, Italy"
+  },
+  {
+    "id": "venice_1965_sandra_(1965_fi",
+    "festival": "venice",
+    "year": 1965,
+    "title_en": "Sandra",
+    "title_cn": "Sandra",
+    "director": "Luchino Visconti",
+    "country": "Italy"
+  },
+  {
+    "id": "venice_1964_red_desert_(fil",
+    "festival": "venice",
+    "year": 1964,
+    "title_en": "Red Desert",
+    "title_cn": "Red Desert",
+    "director": "Michelangelo Antonioni",
+    "country": "Italy"
+  },
+  {
+    "id": "venice_1963_hands_over_the_",
+    "festival": "venice",
+    "year": 1963,
+    "title_en": "Hands over the City",
+    "title_cn": "Hands over the City",
+    "director": "Francesco Rosi",
+    "country": "Italy"
+  },
+  {
+    "id": "venice_1962_family_diary",
+    "festival": "venice",
+    "year": 1962,
+    "title_en": "Family Diary",
+    "title_cn": "Family Diary",
+    "director": "Valerio Zurlini",
+    "country": "Italy"
+  },
+  {
+    "id": "venice_1962_ivan's_childhoo",
+    "festival": "venice",
+    "year": 1962,
+    "title_en": "Ivan's Childhood",
+    "title_cn": "伊凡的少年時代",
+    "director": "Andrei Tarkovsky",
+    "country": "Soviet Union"
+  },
+  {
+    "id": "venice_1961_last_year_at_ma",
+    "festival": "venice",
+    "year": 1961,
+    "title_en": "Last Year at Marienbad",
+    "title_cn": "去年在馬倫巴",
+    "director": "Alain Resnais",
+    "country": "France"
+  },
+  {
+    "id": "venice_1960_tomorrow_is_my_",
+    "festival": "venice",
+    "year": 1960,
+    "title_en": "Tomorrow Is My Turn",
+    "title_cn": "Tomorrow Is My Turn",
+    "director": "André Cayatte",
+    "country": "France"
+  },
+  {
+    "id": "venice_1959_general_della_r",
+    "festival": "venice",
+    "year": 1959,
+    "title_en": "General Della Rovere",
+    "title_cn": "General Della Rovere",
+    "director": "Roberto Rossellini",
+    "country": "France, Italy"
+  },
+  {
+    "id": "venice_1959_the_great_war_(",
+    "festival": "venice",
+    "year": 1959,
+    "title_en": "The Great War",
+    "title_cn": "The Great War",
+    "director": "Mario Monicelli",
+    "country": "France, Italy"
+  },
+  {
+    "id": "venice_1958_rickshaw_man",
+    "festival": "venice",
+    "year": 1958,
+    "title_en": "Rickshaw Man",
+    "title_cn": "Rickshaw Man",
+    "director": "Hiroshi Inagaki",
+    "country": "Japan"
+  },
+  {
+    "id": "venice_1957_aparajito",
+    "festival": "venice",
+    "year": 1957,
+    "title_en": "Aparajito",
+    "title_cn": "Aparajito",
+    "director": "Satyajit Ray",
+    "country": "India"
+  },
+  {
+    "id": "venice_1955_ordet",
+    "festival": "venice",
+    "year": 1955,
+    "title_en": "Ordet",
+    "title_cn": "諾言",
+    "director": "Carl Theodor Dreyer",
+    "country": "Denmark"
+  },
+  {
+    "id": "venice_1954_romeo_and_julie",
+    "festival": "venice",
+    "year": 1954,
+    "title_en": "Romeo and Juliet",
+    "title_cn": "Romeo and Juliet",
+    "director": "Renato Castellani",
+    "country": "United Kingdom"
+  },
+  {
+    "id": "venice_1952_forbidden_games",
+    "festival": "venice",
+    "year": 1952,
+    "title_en": "Forbidden Games",
+    "title_cn": "禁忌的遊戲",
+    "director": "René Clément",
+    "country": "France"
+  },
+  {
+    "id": "venice_1951_rashomon",
+    "festival": "venice",
+    "year": 1951,
+    "title_en": "Rashomon",
+    "title_cn": "羅生門",
+    "director": "Akira Kurosawa",
+    "country": "Japan"
+  },
+  {
+    "id": "venice_1950_justice_is_done",
+    "festival": "venice",
+    "year": 1950,
+    "title_en": "Justice Is Done",
+    "title_cn": "Justice Is Done",
+    "director": "André Cayatte",
+    "country": "France"
+  },
+  {
+    "id": "berlin_2026_yellow_letters",
+    "festival": "berlin",
+    "year": 2026,
+    "title_en": "Yellow Letters",
+    "title_cn": "告知信",
+    "director": "İlker Çatak",
+    "country": "Germany, France, Turkey"
+  },
+  {
+    "id": "berlin_2025_dreams_(sex_lov",
+    "festival": "berlin",
+    "year": 2025,
+    "title_en": "Dreams (Sex Love)",
+    "title_cn": "關於夢（性與愛）",
+    "director": "Dag Johan Haugerud",
+    "country": "Norway"
+  },
+  {
+    "id": "berlin_2024_dahomey_(film)",
+    "festival": "berlin",
+    "year": 2024,
+    "title_en": "Dahomey",
+    "title_cn": "達荷美：祖靈回家",
+    "director": "Mati Diop",
+    "country": "France, Senegal, Benin"
+  },
+  {
+    "id": "berlin_2023_on_the_adamant",
+    "festival": "berlin",
+    "year": 2023,
+    "title_en": "On the Adamant",
+    "title_cn": "塞納河上的船屋",
+    "director": "Nicolas Philibert",
+    "country": "France, Japan"
+  },
+  {
+    "id": "berlin_2022_alcarràs_(film)",
+    "festival": "berlin",
+    "year": 2022,
+    "title_en": "Alcarràs",
+    "title_cn": "桃子樹的最後豐收",
+    "director": "Carla Simón",
+    "country": "Spain, Italy"
+  },
+  {
+    "id": "berlin_2021_bad_luck_bangin",
+    "festival": "berlin",
+    "year": 2021,
+    "title_en": "Bad Luck Banging or Loony Porn",
+    "title_cn": "倒楣性愛和瘋狂A片",
+    "director": "Radu Jude",
+    "country": "Romania"
+  },
+  {
+    "id": "berlin_2020_there_is_no_evi",
+    "festival": "berlin",
+    "year": 2020,
+    "title_en": "There Is No Evil",
+    "title_cn": "無邪",
+    "director": "Mohammad Rasoulof",
+    "country": "Iran"
+  },
+  {
+    "id": "berlin_2019_synonyms_(film)",
+    "festival": "berlin",
+    "year": 2019,
+    "title_en": "Synonyms",
+    "title_cn": "出走巴黎",
+    "director": "Nadav Lapid",
+    "country": "France, Israel"
+  },
+  {
+    "id": "berlin_2018_touch_me_not",
+    "festival": "berlin",
+    "year": 2018,
+    "title_en": "Touch Me Not",
+    "title_cn": "禁身接觸",
+    "director": "Adina Pintilie",
+    "country": "Romania"
+  }
+]);
